af2c226b82da528983fe556f503e5dda4f34ffb5
Guilherme <guilhermekfe@outlook.com>
2017-06-22 15:50:10 -0300

    configurações

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   root 'pages#index'
 
   namespace :backoffice do
@@ -7,8 +11,25 @@ Rails.application.routes.draw do
       resources :careers, only: [:index, :new, :create, :edit, :update, :destroy]
       resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
       resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
+      resources :orders, only: [:index, :show]
     end
+
+    resources :dashboard, only: :index
+
+    # Shopping
+    resources :products, only: [:index, :show]
+    resource :cart, only: :show
+    resource :checkout, only: :update
+    resources :order_items, only: [:create, :update, :destroy]
+    resources :orders, only: [:index, :show]
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for(
+    :users,
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
+  )
+
 end

b8bf8da47ba4a23bc325091d3f8dad401addaa2a
Guilherme <guilhermekfe@outlook.com>
2017-06-22 15:52:55 -0300

    form de novo registro

---
+++ app/forms/new_registration_form.rb
@@ -0,0 +1,42 @@
+class NewRegistrationForm < Form
+
+  attribute :sponsor_username
+  attribute :username
+  attribute :name
+  attribute :phone
+  attribute :skype
+  attribute :email
+  attribute :password
+  attribute :password_confirmation
+  attribute :accept_terms, Boolean
+
+  validates :sponsor_username, :username, :name, :phone, :skype, :email, :password,
+            :password_confirmation, presence: true
+  validates :email, email: true
+
+  validate :terms_accepted
+  validate :sponsor_exists
+  validate :username_is_unique
+
+  def sponsor
+    User.find_by(username: sponsor_username)
+  end
+
+  private
+
+  def sponsor_exists
+    return if User.where(username: sponsor_username).any?
+    errors.add(:sponsor_username, 'sponsor doesnt exist')
+  end
+
+  def username_is_unique
+    return unless User.where(username: username).any?
+    errors.add(:username, 'already exists')
+  end
+
+  def terms_accepted
+    return if accept_terms
+    errors.add(:accept_terms, 'must accept terms')
+  end
+
+end

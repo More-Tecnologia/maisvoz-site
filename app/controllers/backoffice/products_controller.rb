271ddb6fe1dd33cbc1736272a9a5db4aa102d8f1
Guilherme <guilhermekfe@outlook.com>
2017-07-24 15:50:26 -0300

    filtra por categoria de produtos

--- app/controllers/backoffice/products_controller.rb
+++ app/controllers/backoffice/products_controller.rb
@@ -2,12 +2,22 @@ module Backoffice
   class ProductsController < BackofficeController
 
     def index
-      @products = Product.active
+      render(:index, locals: { products: products, q: q })
     end
 
     def show
       @product = Product.find(params[:id])
     end
-  end
 
+    private
+
+    def products
+      @products ||= q.result.page(params[:page])
+    end
+
+    def q
+      @q ||= Product.active.ransack(params[:q])
+    end
+
+  end
 end

76a605f7bb53fa28bfedc8c649c6d03bd5bf1f1e
Guilherme <guilhermekfe@outlook.com>
2017-07-29 12:40:05 -0300

    seeds de primeiros usuários

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+master = User.new(username: 'guilhermekfe', email: 'guilhermekfe@outlook.com', password: 111111, role: :admin)
+master.skip_confirmation!
+master.save!
+
+teste1 = User.new(username: 'teste1', email: 'teste1@morenwm.com', password: 111111, sponsor: master)
+teste1.skip_confirmation!
+teste1.save!
+
+master_node = BinaryNode.create!(user: master)
+teste1_node = BinaryNode.create!(user: teste1, sponsored_by: master, parent: master_node)
+
+master_node.update!(right_child: teste1_node)

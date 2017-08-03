bb327f6c403fa68796f3360ba581d3da5a1073a5
Guilherme <guilhermekfe@outlook.com>
2017-08-03 14:29:43 -0300

    desabilita módulo de confirmação, para testes

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,7 +63,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-         :trackable, :validatable, :confirmable, :lockable
+         :trackable, :validatable, :lockable
 
   has_one :cloudinary_image, as: :imageable
   has_one :account

496abf5d15e6a0bfd000692d7330839fa7bd36ae
Guilherme <guilhermekfe@outlook.com>
2018-04-02 19:32:03 -0300

    remove footable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,13 +17,11 @@
 //= require 'autonumeric/dist/autoNumeric.min'
 //= require twitter-bootstrap-wizard/jquery.bootstrap.wizard
 //= require jquery-mask-plugin/dist/jquery.mask
-//= require footable/dist/footable.all
 //= require notifyjs
 //= require bootstrap-datepicker
 //= require raphael
 //= require product-form-kind
 //= require withdraw-simulator
-//= require table-filtering
 //= require zipcode-fill
 //= require rails.validations
 //= require rails.validations.simple_form

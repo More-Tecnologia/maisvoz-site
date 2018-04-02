9750d2f8ed27363d9cc6da0bceb3b055364c1b3b
Guilherme <guilhermekfe@outlook.com>
2018-04-02 09:17:53 -0300

    melhorias no layout

--- app/controllers/backoffice/accumulated_pva_controller.rb
+++ app/controllers/backoffice/accumulated_pva_controller.rb
@@ -2,7 +2,17 @@ module Backoffice
   class AccumulatedPvaController < BackofficeController
 
     def index
-      render(:index)
+      render(:index, locals: { sponsored: sponsored, user: user })
+    end
+
+    private
+
+    def user
+      current_user.decorate
+    end
+
+    def sponsored
+      @sponsored ||= current_user.sponsored.order(pva_total: :desc).decorate
     end
 
   end

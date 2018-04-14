1f1b354ca4cd4317885a7545cf293bf87ee75685
guilhermekfe <guilhermekfe@outlook.com>
2018-04-14 20:31:43 -0300

    corrige regra do bônus de atividade

--- app/services/calculate_user_activity_bonus_range.rb
+++ app/services/calculate_user_activity_bonus_range.rb
@@ -7,7 +7,7 @@ class CalculateUserActivityBonusRange
   def call
     if pv_activity_sum >= 700 && pv_activity_sum < 2800
       0.02
-    elsif pv_activity_sum >= 2100 && pv_activity_sum < 4200
+    elsif pv_activity_sum >= 2800 && pv_activity_sum < 4200
       0.04
     elsif pv_activity_sum >= 4200
       0.06
@@ -23,9 +23,19 @@ class CalculateUserActivityBonusRange
   attr_reader :user
 
   def pv_activity_sum
-    @pv_activity_sum ||= user.orders.where(
+    @pv_activity_sum ||= (my_orders_pva + my_clients_pva) || 0
+  end
+
+  def my_orders_pva
+    user.orders.where(
       'created_at > ?', 30.days.ago.beginning_of_day
-    ).where(status: :completed).sum(:pv_total) || 0
+    ).where(status: :completed).sum(:pv_total)
+  end
+
+  def my_clients_pva
+    Order.where(user: user.sponsored.consumidor).where(
+      'created_at > ?', 30.days.ago.beginning_of_day
+    ).where(status: :completed).sum(:pv_total)
   end
 
   def pv_semester_activity_sum

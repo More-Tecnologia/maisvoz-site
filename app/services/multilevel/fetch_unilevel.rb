d0f4deea8b41c39a304d5d56107e454d3dba8abd
guilhermekfe <guilhermekfe@outlook.com>
2018-04-16 22:59:20 -0300

    mostra atividade e carreira dos indicados no unilevel

--- app/services/multilevel/fetch_unilevel.rb
+++ app/services/multilevel/fetch_unilevel.rb
@@ -33,7 +33,7 @@ module Multilevel
       return [] if ids.blank?
 
       children = User.where(sponsor_id: ids).pluck(:id)
-      sponsors = User.where(sponsor_id: ids).pluck(:username)
+      sponsors = User.where(sponsor_id: ids).pluck(:username, :active, :career_kind)
 
       return [] if children.size.zero?
 

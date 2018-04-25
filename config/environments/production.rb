6cae2d460ed01766747e1f737f6a1d712431016e
guilhermekfe <guilhermekfe@outlook.com>
2018-04-25 10:37:32 -0300

    atualiza configuração default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,5 +99,5 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { host: 'futuremotors-production.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: ENV['BASE_HOST'], protocol: ENV['BASE_HOST_PROTOCOL'] }
 end

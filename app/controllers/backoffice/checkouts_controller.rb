4e0ccff04e80dc4250e571a7ee24e6ee3b121189
guilhermekfe <guilhermekfe@outlook.com>
2018-04-29 16:31:13 -0300

    evita que o carrinho seja finalizado com uma compra vazia
    fixup! evita que o carrinho seja finalizado com uma compra vazia

--- app/controllers/backoffice/checkouts_controller.rb
+++ app/controllers/backoffice/checkouts_controller.rb
@@ -2,7 +2,8 @@ module Backoffice
   class CheckoutsController < BackofficeController
 
     def update
-      if current_order.pending_payment!
+      if current_order.cart? && current_order.total_cents > 0
+        current_order.pending_payment!
         SlackMessageWorker.perform_async('#loja', "Pedido *#{current_order.hashid}* do usuário *#{current_order.user.username}* no valor de *R$ #{current_order.total}* realizado...")
         Payment::CreateTransaction.new(current_order).call
         ShoppingMailer.with(order: current_order).order_made.deliver_later

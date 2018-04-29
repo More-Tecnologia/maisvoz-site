7e1762694a9806388aebc7ef80cbe7c49fd2e372
guilhermekfe <guilhermekfe@outlook.com>
2018-04-29 16:01:55 -0300

    filtros de nome, cpf e email na tela de admin usuários

--- app/queries/users_query.rb
+++ app/queries/users_query.rb
@@ -6,6 +6,9 @@ class UsersQuery
 
   def call(params)
     scoped = filter_by_username(initial_scope, params[:username])
+    scoped = filter_by_name(scoped, params[:name])
+    scoped = filter_by_email(scoped, params[:email])
+    scoped = filter_by_cpf(scoped, params[:document_cpf])
     scoped = paginate(scoped, params[:page])
     scoped = sort(scoped, params[:sort_type], params[:sort_direction])
     scoped
@@ -17,6 +20,18 @@ class UsersQuery
 
   def filter_by_username(scoped, username)
     username.present? ? scoped.where("username LIKE ?", "%#{username}%") : scoped
+  end
+
+  def filter_by_name(scoped, name)
+    name.present? ? scoped.where("LOWER(name) LIKE LOWER(?)", "%#{name}%") : scoped
+  end
+
+  def filter_by_email(scoped, email)
+    email.present? ? scoped.where(email: email) : scoped
+  end
+
+  def filter_by_cpf(scoped, document_cpf)
+    document_cpf.present? ? scoped.where(document_cpf: document_cpf) : scoped
   end
 
   def paginate(scoped, page = 0)

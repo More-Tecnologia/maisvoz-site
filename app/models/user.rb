3e336126d67d9614b36f39588d3540800579f8fb
guilhermekfe <guilhermekfe@outlook.com>
2018-05-02 17:31:16 -0300

    desabilita confirmação de conta

--- app/models/user.rb
+++ app/models/user.rb
@@ -105,7 +105,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-         :trackable, :validatable, :lockable, :masqueradable, :confirmable
+         :trackable, :validatable, :lockable, :masqueradable
 
   has_attachment :avatar, accept: [:jpg, :png]
   has_attachment :document_rg_photo

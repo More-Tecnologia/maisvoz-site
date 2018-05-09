6cbd2f2ff38a2fc7a9a97d3af739457d09e4d15f
guilhermekfe <guilhermekfe@outlook.com>
2018-05-09 09:22:14 -0300

    reconfigura rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,7 +2,11 @@ redis = ENV['REDISTOGO_URL'] || 'localhost'
 Rack::Attack.cache.store = ActiveSupport::Cache::RedisStore.new(redis)
 
 Rack::Attack.throttle('limit login attempts', limit: 6, period: 60.minutes) do |req|
-  req.ip if (req.path == '/users/sign_in' || req.path == '/admin/login') && req.post?
+  req.params['user']['login'] if req.path == '/users/sign_in' && req.post?
+end
+
+Rack::Attack.throttle('limit admin login attempts', limit: 6, period: 60.minutes) do |req|
+  req.params['admin_user']['login'] if req.path == '/admin/login' && req.post?
 end
 
 Rack::Attack.throttled_response = lambda do |env|

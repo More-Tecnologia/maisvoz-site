3657526a053baa89b206162c7301f1956d05c290
guilhermekfe <guilhermekfe@outlook.com>
2018-10-15 10:56:00 -0300

    fix download material

--- app/controllers/backoffice/downloads_controller.rb
+++ app/controllers/backoffice/downloads_controller.rb
@@ -10,14 +10,8 @@ module Backoffice
     def download_links
       [
         { name: 'Cartão de Visita para Empreendedor', url: 'https://www.dropbox.com/s/hydtieuwnkm3g9b/Cart%C3%A3o%20de%20Visita%20para%20Empreendedores.pdf'},
-        { name: 'Folder Multifuncional H-MAX', url: 'https://www.dropbox.com/s/m5o0o9dlq21snns/Folder%20Multifuncional%20H-MAX.pdf'},
-        { name: 'Panfleto H-MAX', url: 'https://www.dropbox.com/s/udi9lrarbs31v7q/Panfleto%20H-MAX.pdf'},
         { name: 'Modelos de Convite para Eventos', url: 'https://www.dropbox.com/s/cilnry06058fjgy/Modelo%20de%20Convite.pdf'},
-        { name: 'Manual do Negócio', url: 'https://www.dropbox.com/s/9atfdjymv4u76fw/Manual%20do%20Plano%20de%20Negocio_20180205.pdf'},
-        { name: 'Plano de Compensação', url: 'https://www.dropbox.com/s/a7kehg1lbyxh3ii/PLANO%20DE%20COMPENSA%C3%87%C3%83O_20180205.pdf'},
         { name: 'Apresentação do Negócio', url: 'https://www.dropbox.com/s/5y9mbk55c9f36b6/APN%20VERS%C3%83O%20COMPLETA.pdf'},
-        { name: 'Instalação do H-max', url: 'https://youtu.be/BDu_fUOtodQ'},
-        { name: 'H-max inovacão tecnológica', url: 'https://youtu.be/RYAupehqGmE'},
         { name: 'Adesivos para Carros - Branco', url: 'https://www.dropbox.com/s/anm8402ogrz4uen/Adesivo%20Branco%20Carro.pdf'},
         { name: 'Adesivos para Carros - Preto', url: 'https://www.dropbox.com/s/rzuf2uivhbjsk4r/Adesivo%20Preto%20Carro.pdf'},
         { name: 'Adesivos para Carros - Espelhado', url: 'https://www.dropbox.com/s/ycrg7zzz2b241oq/Adesivo%20Espelhado%20Carro.pdf'},

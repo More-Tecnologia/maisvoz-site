15f72fe01e8a88f9376fa31f97961657a634335b
Guilherme <guilhermekfe@outlook.com>
2019-01-31 16:08:47 -0200

    Block remove action from tracker subscription

--- app/policies/club_motors_subscription_policy.rb
+++ app/policies/club_motors_subscription_policy.rb
@@ -14,6 +14,7 @@ class ClubMotorsSubscriptionPolicy
   def can_remove?
     club_motors_subscription.inactive? &&
       club_motors_subscription.chassis.blank? &&
+      !club_motors_subscription.tracker? &&
       owner?
   end
 

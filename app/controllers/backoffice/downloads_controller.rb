bb28ce92e746b8440eaccf35d5aea8ffc8aaf2d3
Guilherme <guilhermekfe@outlook.com>
2019-02-11 00:15:09 -0200

    Add social media images to downloads section

--- app/controllers/backoffice/downloads_controller.rb
+++ app/controllers/backoffice/downloads_controller.rb
@@ -2,7 +2,7 @@ module Backoffice
   class DownloadsController < EntrepreneurController
 
     def index
-      render :index, locals: { links: download_links }
+      render :index, locals: { links: download_links, images: images }
     end
 
     private
@@ -19,8 +19,30 @@ module Backoffice
         { name: 'Modelo para o Comece Certo - 1', url: 'https://www.dropbox.com/s/o4dtz7leqqx586g/Modelo%20para%20Comece%20Certo_01.pdf'},
         { name: 'Modelo para o Comece Certo - 2', url: 'https://www.dropbox.com/s/hc0kttojvfguzmt/Modelo%20para%20Comece%20Certo_02.pdf'},
         { name: 'Modelo para o Comece Certo - 3', url: 'https://www.dropbox.com/s/altlgrl581xz9z7/Modelo%20para%20Comece%20Certo_03.pdf'},
-        { name: 'Apresentação', url: 'https://www.dropbox.com/s/gi4rl16kikcedtl/apnfuturemotors.pdf'},
-        { name: 'Plano de Negócios', url: 'https://futuremotors.com.br/wp-content/uploads/2018/10/manual_plano_de_negocios.pdf'}
+        { name: 'Apresentação', url: 'https://s3.amazonaws.com/promotional-material/pdf/APN_FutureMotors.pdf'},
+        { name: 'Plano de Negócios', url: 'https://futuremotors.com.br/wp-content/uploads/2018/10/manual_plano_de_negocios.pdf'},
+      ]
+    end
+
+    def images
+      [
+        'https://s3.amazonaws.com/promotional-material/images/social-media/1.jpg',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/2.png',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/3.png',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/4.png',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/5.png',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/6.png',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/7.png',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/8.jpeg',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/9.jpeg',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/10.jpeg',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/11.jpeg',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/Susp.png',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/correia.png',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/freios.png',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/oleo.png',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/pneus.png',
+        'https://s3.amazonaws.com/promotional-material/images/social-media/vela.png',
       ]
     end
 

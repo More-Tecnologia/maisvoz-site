0f116ee39aa0880243eb75e981030f12e62997a6
Guilherme <guilhermekfe@outlook.com>
2019-03-13 16:10:30 -0300

    Allow only active users to use vouchers

--- app/controllers/backoffice/pay_with_voucher_controller.rb
+++ app/controllers/backoffice/pay_with_voucher_controller.rb
@@ -1,6 +1,8 @@
 module Backoffice
   class PayWithVoucherController < BackofficeController
 
+    before_action :verify_user_active
+
     def new
       render_new
     end
@@ -34,6 +36,12 @@ module Backoffice
       params[:pay_with_voucher_form] ||= {}
       params[:pay_with_voucher_form][:user] ||= current_user
       params[:pay_with_voucher_form]
+    end
+
+    def verify_user_active
+      return if current_user.active?
+
+      redirect_back(fallback_location: root_path, alert: 'Usuários inativos não podem pagar com voucher')
     end
 
   end

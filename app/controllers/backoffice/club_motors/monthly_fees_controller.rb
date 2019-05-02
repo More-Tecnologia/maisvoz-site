515204154bf5dda41aca4a4284a5f3a6926c3e89
Guilherme <guilhermekfe@outlook.com>
2019-05-02 10:06:54 -0300

    Ordena mensalidade por mais recente

--- app/controllers/backoffice/club_motors/monthly_fees_controller.rb
+++ app/controllers/backoffice/club_motors/monthly_fees_controller.rb
@@ -3,7 +3,7 @@ module Backoffice
     class MonthlyFeesController < BackofficeController
 
       def index
-        @monthly_fees = current_user.orders.monthly_fees.includes(:payment_transactions)
+        @monthly_fees = current_user.orders.monthly_fees.order(created_at: :desc).includes(:payment_transactions)
       end
 
     end

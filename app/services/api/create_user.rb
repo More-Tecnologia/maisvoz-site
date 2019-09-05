f99896c4fa1a1d4519973b32a58fe98903f3b094
vinibr3 <vinicius.deoliveira@outlook.com>
2019-09-05 11:48:52 -0300

    Atribui nome de usuário por tipo de registro

--- app/services/api/create_user.rb
+++ app/services/api/create_user.rb
@@ -7,14 +7,15 @@ module Api
 
     def call
       @user = User.new.tap do |user|
-        user.sponsor           = form.sponsor
-        user.username          = form.username
-        user.name              = form.name
-        user.email             = form.email
-        user.document_cpf      = form.cpf
-        user.document_cnpj     = form.cnpj
-        user.password          = form.password
-        user.registration_type = form.registration_type
+        user.registration_type     = form.registration_type
+        user.sponsor               = form.sponsor
+        user.username              = form.username
+        user.name                  = form.name if user.pf?
+        user.email                 = form.email
+        user.document_cpf          = form.cpf
+        user.document_cnpj         = form.cnpj
+        user.password              = form.password
+        user.document_company_name = form.name if user.pj?
 
         user.save!
       end

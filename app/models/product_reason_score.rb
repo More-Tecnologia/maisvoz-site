2647b6a701289fb1f4090ef87fc0830920aad918
vinibr3 <vinicius.deoliveira@outlook.com>
2019-10-05 09:43:32 -0300

    Remove product_score ref from ProductReasonScore

--- app/models/product_reason_score.rb
+++ app/models/product_reason_score.rb
@@ -1,5 +1,4 @@
 class ProductReasonScore < ApplicationRecord
   belongs_to :product
   belongs_to :financial_reason
-  belongs_to :product_score
 end
--- spec/models/product_reason_score_spec.rb
+++ spec/models/product_reason_score_spec.rb
@@ -3,5 +3,4 @@ require 'rails_helper'
 RSpec.describe ProductReasonScore, type: :model do
   it { is_expected.to belong_to(:product) }
   it { is_expected.to belong_to(:financial_reason) }
-  it { is_expected.to belong_to(:product_score) }
 end

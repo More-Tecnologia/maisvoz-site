1432fd4d2dab72e7398bfafd652b24de820a209b
vinibr3 <vinicius.deoliveira@outlook.com>
2019-10-08 10:08:51 -0300

    Score admin index page

--- app/controllers/backoffice/scores_controller.rb
+++ app/controllers/backoffice/scores_controller.rb
@@ -3,14 +3,8 @@ module Backoffice
     def index
       @q = Score.ransack(params[:q])
       @scores = @q.result(distinct: true)
-                  .by_tree_types(valid_tree_types)
+                  .includes_associations
                   .page(params[:page])
-    end
-
-    private
-
-    def valid_tree_types
-      ScoreType.tree_types.slice(*params[:tree_types]).values
     end
   end
 end

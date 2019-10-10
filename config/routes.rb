c3c5f08c13fa6eaeb4895221c5f3d94df851df83
vinibr3 <vinicius.deoliveira@outlook.com>
2019-10-10 19:03:08 -0300

    Remove transfers

--- config/routes.rb
+++ config/routes.rb
@@ -66,7 +66,6 @@ Rails.application.routes.draw do
     end
 
     # Financial
-    # resources :transfers, only: [:show, :update, :create]
     resources :withdrawals, only: [:index, :new, :create]
     resources :financial_transactions, only: [:index]
     resources :bonus_entries, only: [:index]

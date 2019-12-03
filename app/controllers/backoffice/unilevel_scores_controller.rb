ef0c0b851c73705e6185be2996a071cb333dd391
Developer 02 <aurelio.lmoreira@gmail.com>
2019-12-03 16:53:05 -0300

    Score Balance

--- app/controllers/backoffice/unilevel_scores_controller.rb
+++ app/controllers/backoffice/unilevel_scores_controller.rb
@@ -2,13 +2,18 @@ module Backoffice
   class UnilevelScoresController < EntrepreneurController
 
     def index
-      @q = Score.ransack(params[:q])
-      @scores = @q.result(distinct: true)
-                  .unilevel_by_user(current_user)
+      @q = Score.ransack(query_params)
+      @scores = @q.result
+                  .includes(:spreader_user, :order, :score_type)
+                  .order(created_at: :desc)
                   .page(params[:page])
-      @tree_type = 'unilevel'
-      render template: 'backoffice/scores/index'
     end
-    
+
+    def query_params
+      query = params[:q] ? params[:q].to_hash.symbolize_keys : {}
+      query.merge!(score_type_id_in: ScoreType.unilevel.pluck(:id)) unless filled?(query[:score_type_id_in])
+      query.merge!(user_id_eq: current_user.id)
+    end
+
   end
 end

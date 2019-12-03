fbfb3c37e3b910bbd8334fb52860aa56d95293a1
Developer 02 <aurelio.lmoreira@gmail.com>
2019-12-03 10:22:23 -0300

    Fix threshold date

--- app/services/financial/unlock_blocked_balance.rb
+++ app/services/financial/unlock_blocked_balance.rb
@@ -30,15 +30,15 @@ module Financial
     end
 
     def threshold_reached?
-      user.blocked_balance_cents / user.current_trail.product.price_cents * 1e2 >=
-        user.current_career_trail.unlock_blocked_balance_threshold
+      product_value = user.current_trail.product.product_value
+      rate = user.blocked_balance_cents.to_f / product_value if product_value
+      threshold_rate = user.current_career_trail.unlock_blocked_balance_threshold.to_f / 100.0
+      rate.to_f > threshold_rate.to_f
     end
 
     def minimum_period_reached?
-      product_value = user.current_trail.product.product_value
-      rate = user.blocked_balance_cents.to_f / product_value if product_value > 0
-      threshold_rate = user.current_career_trail.unlock_blocked_balance_threshold.to_f / 100.0
-      rate.to_f > threshold_rate.to_f
+      threshold_time = user.current_career_trail.unlock_blocked_balance_min_period.days.ago
+      threshold_time >= user.balance_unlocked_at if user.balance_unlocked_at
     end
 
   end

89fd8a494e639d63ff87b3f489cb6f0d12de42cd
vinibr3 <vinicius.deoliveira@outlook.com>
2019-12-17 10:54:30 -0300

    Indentify sim cards added

--- app/controllers/backoffice/admin/sim_card_controls_controller.rb
+++ app/controllers/backoffice/admin/sim_card_controls_controller.rb
@@ -4,7 +4,7 @@ module Backoffice
 
       def new
         @q = OrderItem.search(params[:q])
-        @sim_card_order_items = @q.result.includes(:product, order: [:user])
+        @sim_card_order_items = @q.result.includes(:product, :sim_cards, order: [:user])
                                          .sim_card
                                          .paid
                                          .order(:created_at, processed_at: :desc)

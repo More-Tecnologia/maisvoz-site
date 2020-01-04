998cc17a39dea8556c23d8b88d4d2e2da6dbb013
vinibr3 <vinicius.deoliveira@outlook.com>
2020-01-04 19:01:38 -0300

    Assign sim card status before validation

--- app/models/sim_card.rb
+++ app/models/sim_card.rb
@@ -19,15 +19,16 @@ class SimCard < ApplicationRecord
 
   before_validation :cleasing
   before_validation :assign_report_dates
+  before_validation :assign_status
 
   scope :by_support_point, ->(user) { where(support_point_user: user) }
   scope :available, -> { where(user: nil) }
   scope :by_iccids, ->(iccids) { where(iccid: iccids) }
+  scope :by_user, ->(user) { where(user: user) }
 
   scope :by_support_point, ->(user) { where(support_point_user: user) }
   scope :available, -> { where(user: nil) }
   scope :by_iccids, ->(iccids) { where(iccid: iccids) }
-  scope :transfered, -> { where.not(support_point_stock_out_at: nil) }
 
   private
 
@@ -38,10 +39,21 @@ class SimCard < ApplicationRecord
 
   def assign_report_dates
     self[:support_point_stock_in_at] = Time.current if support_point_user && !support_point_stock_in_at
-    self[:support_point_stock_out_at] = Time.current if user && !support_point_stock_out_at
+    self[:support_point_stock_out_at] = Time.current if user && support_point_user && !support_point_stock_out_at
     self[:user_stock_in_at] = Time.current if user && !user_stock_in_at
     self[:user_stock_out_at] = Time.current if actived_at && !user_stock_out_at
-    self[:stock_out_at] = self[:support_point_stock_in_at] || self[:user_stock_in_at] if !stock_out_at
+    self[:stock_out_at] = Time.current if phone_number.present? && stock_out_at.nil?
+  end
+
+  def assign_status
+    self[:status] = SimCard.statuses[detect_status]
+  end
+
+  private
+
+  def detect_status
+    return :transfered if user && support_point_user && !phone_number
+    phone_number.present? ? :out_stock : :in_stock
   end
 
 end

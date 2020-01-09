ab082a52083af81ba8362f1bac7dba4fc845d627
vinibr3 <vinicius.deoliveira@outlook.com>
2020-01-09 16:18:32 -0300

    Show unilevel users by careers

--- app/controllers/backoffice/unilevel_controller.rb
+++ app/controllers/backoffice/unilevel_controller.rb
@@ -3,13 +3,12 @@ module Backoffice
 
     def index
       @current_node = current_user.unilevel_node
-      unilevel_nodes = @current_node.subtree
+      @unilevel_nodes = @current_node.subtree
                                     .from_depth(@current_node.depth)
                                     .to_depth(unilevel_max_depth)
                                     .includes(user: [career_trails: [:career]])
                                     .where.not(username: current_user.username)
-                                    .arrange
-      @unilevel_nodes = UnilevelNode.sort_by_ancestry(unilevel_nodes)
+                                    .group_by(&:ancestry_depth)
     end
 
     private

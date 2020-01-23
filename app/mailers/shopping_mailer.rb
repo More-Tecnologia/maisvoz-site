b2148f4dcb9bb846923b036233e514c26cfc7eb5
vinibr3 <vinicius.deoliveira@outlook.com>
2020-01-23 15:33:10 -0300

    fix mailes sending

--- app/mailers/shopping_mailer.rb
+++ app/mailers/shopping_mailer.rb
@@ -1,11 +1,5 @@
 class ShoppingMailer < ApplicationMailer
 
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.shopping_mailer.order_made.subject
-  #
   def order_made
     category('order_made')
 

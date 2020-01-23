07015048ae1d5ed24995afd260f35459196cbe6a
vinibr3 <vinicius.deoliveira@outlook.com>
2020-01-23 19:13:08 -0300

    Send upgrade career email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,14 @@
 class UserMailer < ApplicationMailer
+
   def welcome(user)
     @user = user
     mail to: @user.email
   end
+
+  def upgrade_career(user, new_career)
+    @user = user
+    @new_career = new_career
+    mail to: @user.email, subject: I18n.t('user_mailer.upgrade_career.subject')
+  end
+
 end

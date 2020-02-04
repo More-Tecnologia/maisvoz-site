50fc57ba540a9fd71ae8289d8edf856171f31253
vinibr3 <vinicius.deoliveira@outlook.com>
2020-02-03 23:37:42 -0300

    wallet address

--- app/controllers/backoffice/withdrawals_controller.rb
+++ app/controllers/backoffice/withdrawals_controller.rb
@@ -1,8 +1,8 @@
 module Backoffice
   class WithdrawalsController < EntrepreneurController
 
-    before_action :should_be_verified
     before_action :ensure_bank_account_valid
+    before_action :unavailable_withdrawal
 
     def index
       @withdrawals = current_user.withdrawals
@@ -32,6 +32,10 @@ module Backoffice
       params.require(:withdrawal_form)
             .permit(:amount, :fiscal_document_link, :fiscal_document_photo)
             .merge(user: current_user)
+    end
+
+    def unavailable_withdrawal
+      redirect_to backoffice_dashboard_index_path, alert: 'Withdrawal unavailable yet'
     end
 
   end

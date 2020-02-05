a37c4c37db072df5005ee57a33fcad51b41916c1
vinibr3 <vinicius.deoliveira@outlook.com>
2020-02-05 16:51:36 -0300

    Create Points Qualifications

---
+++ app/controllers/backoffice/point_qualifications_controller.rb
@@ -0,0 +1,22 @@
+module Backoffice
+  class PointQualificationsController < EntrepreneurController
+
+    def index
+      @q = Score.ransack(query_params)
+      @scores = @q.result
+                  .includes(:spreader_user, :order)
+                  .order(created_at: :desc)
+                  .page(params[:page])
+    end
+
+    private
+
+    def query_params
+      query = params[:q] ? params[:q].to_hash.symbolize_keys : {}
+      query.merge!(source_leg_eq: Score.source_legs[:left]) unless filled?(query[:source_leg_eq])
+      query.merge!(score_type_id_in: ScoreType.binary_score.id) unless filled?(query[:score_type_id_in])
+      query.merge!(user_id_eq: current_user.id)
+    end
+
+  end
+end

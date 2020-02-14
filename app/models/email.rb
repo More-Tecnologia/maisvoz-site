32bc1f29c2af3d5b4ad2a5bd60ea5e8450447795
heirian <26394609+Heirian@users.noreply.github.com>
2020-02-14 11:54:14 -0300

    Updates email model and adds validates and callbacks

--- app/models/email.rb
+++ app/models/email.rb
@@ -5,5 +5,28 @@ class Email < ApplicationRecord
 
   belongs_to :user
 
-  validates :body, presence: true, email: true
+  validates :body, presence: true, email: true, uniqueness: { if: :any_active? }
+
+  after_update :change_user_email, if: :active?
+  after_update :inactive_emails, if: :active?
+
+  def change_user_email
+    user.update(email: body)
+  end
+
+  def inactive_emails
+    other_user_emails.each(&:inactivate!)
+  end
+
+  def any_active?
+    self.class.where(body: body, status: 1).any?
+  end
+
+  def other_user_emails
+    user.emails - [self]
+  end
+
+  def inactivate!
+    update_attribute(:status, :inactive)
+  end
 end

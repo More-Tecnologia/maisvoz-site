03e7f1b83e85b54cf9394959bd6a92e37cbc177b
heirian <26394609+Heirian@users.noreply.github.com>
2020-02-27 12:06:35 -0300

    Adds unilevel counts presenter

--- app/decorators/dashboard_user_decorator.rb
+++ app/decorators/dashboard_user_decorator.rb
@@ -1,14 +1,12 @@
 class DashboardUserDecorator
   def initialize(user)
     @user = user
-    @children = @user.unilevel_node.children
     @binary_score = left_binary_score > right_binary_score ? I18n.t(:right) : I18n.t(:left)
   end
 
   def build
     {
       data: {
-        unilevel_counts: unilevel_counts,
         binary_count: binary_count,
         binary_scores: binary_scores
       },
@@ -18,10 +16,7 @@ class DashboardUserDecorator
         binary_affiliates_right_count: I18n.t(:binary_affiliates_right_count),
         binary_scores: I18n.t(:binary_scores),
         left_binary_score: I18n.t(:left_binary_score),
-        right_binary_score: I18n.t(:right_binary_score),
-        unilevel_affiliates_count: I18n.t(:unilevel_affiliates_count),
-        unilevel_affiliates_actives_count: I18n.t(:unilevel_affiliates_actives_count),
-        unilevel_affiliates_inactives_count: I18n.t(:unilevel_affiliates_inactives_count)
+        right_binary_score: I18n.t(:right_binary_score)
       }
     }
   end
@@ -65,25 +60,4 @@ class DashboardUserDecorator
   def right_binary_score
     @user.binary_node.right_leg_accumulated_score
   end
-
-  def unilevel_counts
-    {
-      unilevel_affiliates_count: unilevel_affiliates_count,
-      unilevel_affiliates_actives_count: unilevel_affiliates_actives_count,
-      unilevel_affiliates_inactives_count: unilevel_affiliates_inactives_count
-    }
-  end
-
-  def unilevel_affiliates_count
-    @children.count
-  end
-
-  def unilevel_affiliates_actives_count
-    @children.includes(:user).where(user: User.active).count
-  end
-
-  def unilevel_affiliates_inactives_count
-    @children.includes(:user).where(user: User.inactive).count
-  end
-
 end

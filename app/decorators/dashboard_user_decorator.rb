75063e7acb823ef59e5f90a6f801e13e1a72f1aa
heirian <26394609+Heirian@users.noreply.github.com>
2020-02-27 12:18:17 -0300

    Adds binary counts presenter

--- app/decorators/dashboard_user_decorator.rb
+++ app/decorators/dashboard_user_decorator.rb
@@ -7,41 +7,13 @@ class DashboardUserDecorator
   def build
     {
       data: {
-        binary_count: binary_count,
         binary_scores: binary_scores
       },
       labels: {
-        binary_affiliates_count: I18n.t(:binary_affiliates_count),
-        binary_affiliates_left_count: I18n.t(:binary_affiliates_left_count),
-        binary_affiliates_right_count: I18n.t(:binary_affiliates_right_count),
         binary_scores: I18n.t(:binary_scores),
         left_binary_score: I18n.t(:left_binary_score),
         right_binary_score: I18n.t(:right_binary_score)
       }
-    }
-  end
-
-  def binary_affiliates_count
-    @user.binary_node.descendants.count
-  end
-
-  def binary_affiliates_left_count
-    return 0 unless @user.binary_node.left_child.present?
-
-    @user.binary_node.left_child.descendants.count + 1
-  end
-
-  def binary_affiliates_right_count
-    return 0 unless @user.binary_node.right_child.present?
-
-    @user.binary_node.right_child.descendants.count + 1
-  end
-
-  def binary_count
-    {
-      binary_affiliates_count: binary_affiliates_count,
-      binary_affiliates_left_count: binary_affiliates_left_count,
-      binary_affiliates_right_count: binary_affiliates_right_count
     }
   end
 

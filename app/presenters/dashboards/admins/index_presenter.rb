1d753ebb41fa40ab367e0c09c1889be5ebc000ef
heirian <26394609+Heirian@users.noreply.github.com>
2020-02-28 11:32:10 -0300

    Adds dashboard admin index presenter

---
+++ app/presenters/dashboards/admins/index_presenter.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module Dashboards
+  module Admins
+    class IndexPresenter
+
+      def active_products_count
+        Product.active.count
+      end
+
+      def data_list
+        [
+          { label: I18n.t('views.dashboard.admin.today_orders'),
+            value: today_orders_count },
+          { label: I18n.t('views.dashboard.admin.total_orders'),
+            value: orders_count },
+          { label: I18n.t('views.dashboard.admin.active_products'),
+            value: active_products_count },
+          { label: I18n.t('views.dashboard.admin.total_users'),
+            value: users_count }
+        ]
+      end
+
+      def last_orders
+        Order.last(10).reverse
+      end
+
+      def last_orders_count
+        Order.created_after(10)
+             .group_by_day(:created_at)
+             .count
+      end
+
+      def last_users_count
+        User.created_after(10)
+            .group_by_day(:created_at)
+            .count
+      end
+
+      def orders_count
+        Order.valids.count
+      end
+
+      def today_orders_count
+        Order.valids.today.count
+      end
+
+      def users_count
+        User.count
+      end
+
+    end
+  end
+end

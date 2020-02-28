017c8c3e76400c8d31fa4868cc5746c28b3f9b0a
heirian <26394609+Heirian@users.noreply.github.com>
2020-02-28 11:33:35 -0300

    Updates admin dashboard to use index presenter

--- app/controllers/backoffice/admin/dashboard_controller.rb
+++ app/controllers/backoffice/admin/dashboard_controller.rb
@@ -3,14 +3,9 @@
 module Backoffice
   module Admin
     class DashboardController < EntrepreneurController
+
       def index
-        @last_orders = Order.order(created_at: :desc).first(10)
-        @last_users_count = User.where('created_at >= ?', 10.days.ago.beginning_of_day).group_by_day(:created_at).count
-        @last_orders_count = Order.where('created_at >= ?', 10.days.ago.beginning_of_day).group_by_day(:created_at).count
-        @active_products_count = Product.active.count
-        @users_count = User.count
-        @orders_count = Order.where.not(status: Order.statuses[:cart]).count
-        @today_orders_count = Order.where.not(status: Order.statuses[:cart]).or(Order.where.not(status: Order.statuses[:expired])).today.count
+        @index = Dashboards::Admins::IndexPresenter.new
       end
 
       def bonus_data

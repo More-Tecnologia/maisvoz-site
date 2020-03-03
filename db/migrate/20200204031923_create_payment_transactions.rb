013b58c81a50e2a0ba328599dd402a81aa2e437f
vinibr3 <vinicius.deoliveira@outlook.com>
2020-03-03 09:38:24 -0300

    Remove drop table payment transactions from migrations

--- db/migrate/20200204031923_create_payment_transactions.rb
+++ db/migrate/20200204031923_create_payment_transactions.rb
@@ -1,14 +1,13 @@
 class CreatePaymentTransactions < ActiveRecord::Migration[5.2]
-  # def change
-  #   drop_table :payment_transactions
-  #   create_table :payment_transactions do |t|
-  #     t.references :order
-  #     t.integer    :status, default: 0
-  #     t.string     :transaction_id, unique: true, index: true
-  #     t.decimal    :amount
-  #     t.text       :provider_response
-  #
-  #     t.timestamps
-  #   end
-  # end
+  def change
+    create_table :payment_transactions do |t|
+      t.references :order
+      t.integer    :status, default: 0
+      t.string     :transaction_id, unique: true, index: true
+      t.decimal    :amount
+      t.text       :provider_response
+
+      t.timestamps
+    end
+  end
 end

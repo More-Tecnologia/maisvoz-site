14ff9d7d1d533f8edd342d136f290d72404bef4f
heirian <26394609+Heirian@users.noreply.github.com>
2020-03-06 17:43:43 -0300

    Adds landing page folder to assets precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( charts.js recaptcha.js )
-Rails.application.config.assets.precompile += %w( dashboards/user_dashboard.js )
+Rails.application.config.assets.precompile += %w( dashboards/user_dashboard.js landing_page/*)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets

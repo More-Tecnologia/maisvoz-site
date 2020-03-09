e6e25f7e030200676c3a9ee37937536ff878614f
heirian <26394609+Heirian@users.noreply.github.com>
2020-03-09 17:56:45 -0300

    Updates root path to signed in users

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -8,13 +8,13 @@ class LandingPagesController < ApplicationController
   def index; end
 
   def ensure_signed_out
-    redirect_to backoffice_products_path if user_signed_in?
+    redirect_to backoffice_dashboard_index_path if user_signed_in?
   end
 
   def ensure_sponsor
-    if params[:sponsor].present?
-      @sponsor = User.find_by(username: params[:sponsor])
-    end
+    return unless params[:sponsor].present?
+
+    @sponsor = User.find_by(username: params[:sponsor])
   end
 
 end

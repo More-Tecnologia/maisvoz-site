af47a9b952e915005d4c75cbb5f93723efbc7988
vinibr3 <vinicius.deoliveira@outlook.com>
2020-04-24 15:00:38 -0300

    Create order payment type to payments no bonus

--- app/presenters/dashboards/admins/payment_presenter.rb
+++ app/presenters/dashboards/admins/payment_presenter.rb
@@ -17,6 +17,7 @@ module Dashboards
         {
           balance: Order.balance.count,
           admin: Order.admin.count,
+          admin_nb: Order.admin_nb.count,
           voucher: Order.voucher.count,
           btc: Order.btc.count,
           total_payments: Order.completed.count
@@ -27,6 +28,7 @@ module Dashboards
         {
           balance: I18n.t(:balance),
           admin: I18n.t(:admin),
+          admin_nb: I18n.t(:admin_nb),
           voucher: I18n.t(:voucher),
           btc: I18n.t(:btc),
           total_payments: I18n.t(:total_payments)

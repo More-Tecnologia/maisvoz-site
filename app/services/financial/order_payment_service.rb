905980f3f7a457554c042362f21ca6c6ddc7fa11
vinibr3 <vinicius.deoliveira@outlook.com>
2020-04-24 15:02:24 -0300

    Create financial reason for payments no bonification

--- app/services/financial/order_payment_service.rb
+++ app/services/financial/order_payment_service.rb
@@ -4,7 +4,7 @@ module Financial
     def call
       user = User.find_morenwm_customer_admin
       user.financial_transactions.create!(spreader: order.user,
-                                          financial_reason: FinancialReason.order_payment,
+                                          financial_reason: detect_financial_reason,
                                           cent_amount: total,
                                           order: order)
     end
@@ -15,10 +15,15 @@ module Financial
 
     def initialize(args)
       @order = args[:order]
+      @enabled_bonification = args[:enabled_bonification]
     end
 
     def total
       order.total_cents / 100.0
+    end
+
+    def detect_financial_reason
+      @enabled_bonification ? FinancialReason.order_payment : FinancialReason.order_sponsored
     end
 
   end

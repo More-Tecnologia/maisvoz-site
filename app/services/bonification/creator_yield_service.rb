7d2848da32bdb342d3005e8412a22eb328225f4b
vinibr3 <vinicius.deoliveira@outlook.com>
2020-05-16 13:04:40 -0300

    Send email error to admin at process yield bonus

--- app/services/bonification/creator_yield_service.rb
+++ app/services/bonification/creator_yield_service.rb
@@ -1,9 +1,13 @@
 module Bonification
   class CreatorYieldService < ApplicationService
     def call
+      errors = []
       yield_contracts.find_each do |contract|
         create_yield_bonus_for(contract) if contract.rentability.positive?
+      rescue StandardError => error
+        errors << error
       end
+      notify_admin_by_email(errors) if errors.any?
     end
 
     private
@@ -22,6 +26,11 @@ module Bonification
                    moneyflow: :credit,
                    cent_amount: contract.rentability,
                    bonus_contract: contract)
+    end
+
+    def notify_admin_by_email(errors)
+      subject = "Yield Bonus Errors: #{errors.size}"
+      ErrorsMailer.notify_admin(subject, errors).deliver_later
     end
   end
 end

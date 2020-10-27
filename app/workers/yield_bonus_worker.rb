adf9be0711c3271d5960de9bcccb794da37cf86d
vinibr3 <vinicius.deoliveira@outlook.com>
2020-10-27 20:11:33 -0300

    Run publicity bonus on beginning of day

--- app/workers/yield_bonus_worker.rb
+++ app/workers/yield_bonus_worker.rb
@@ -5,6 +5,6 @@ class YieldBonusWorker
     monday_to_friday = (2..6).to_a
     Bonification::CreatorYieldService.call if Date.current.wday.in?(monday_to_friday)
 
-    YieldBonusWorker.perform_at(Date.tomorrow.end_of_day)
+    YieldBonusWorker.perform_at(Date.tomorrow.beginning_of_day)
   end
 end

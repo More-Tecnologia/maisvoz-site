69b6ddce332d19be3befcd58614f331aa94ce021
vinibr3 <vinicius.deoliveira@outlook.com>
2020-11-02 11:16:37 -0300

    Pay yield bonus from thursday to saturday

--- app/workers/yield_bonus_worker.rb
+++ app/workers/yield_bonus_worker.rb
@@ -2,8 +2,8 @@ class YieldBonusWorker
   include Sidekiq::Worker
 
   def perform
-    monday_to_friday = (1..5).to_a
-    Bonification::CreatorYieldService.call if Date.current.wday.in?(monday_to_friday)
+    thursday_to_saturday = (2..6).to_a
+    Bonification::CreatorYieldService.call if Date.current.wday.in?(thursday_to_saturday)
 
     YieldBonusWorker.perform_at(Date.tomorrow.beginning_of_day)
   end

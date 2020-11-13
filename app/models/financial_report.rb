ba2ec9e754d9360e0322ec61fc799fbd8b924f4f
vinibr3 <vinicius.deoliveira@outlook.com>
2020-11-13 20:57:03 -0300

    Partner amount

--- app/models/financial_report.rb
+++ app/models/financial_report.rb
@@ -1,7 +1,7 @@
 class FinancialReport < ApplicationRecord
   POOL_PERCENTAGE = 75
   PROFIT_PERCENTAGE = 25
-  PARTNER_PROFIT_PERCENTAGE = 5
+  PARTNER_COUNT = 5.0
 
   validates_presence_of :order_payment_amount_cents, :pool_amount_cents,
                         :withdrawal_amount_cents, :pool_balance_amount_cents,
@@ -25,7 +25,7 @@ class FinancialReport < ApplicationRecord
     self.pool_amount_cents = order_payment_amount_cents * POOL_PERCENTAGE / 100.0
     self.pool_balance_amount_cents = pool_amount_cents - withdrawal_amount_cents
     self.profit_amount_cents = order_payment_amount_cents * PROFIT_PERCENTAGE / 100.0
-    self.profit_per_partner_amount_cents = profit_amount_cents * PARTNER_PROFIT_PERCENTAGE / 100.0
+    self.profit_per_partner_amount_cents = profit_amount_cents / PARTNER_COUNT
   end
 
   def self.current_week_order_payment_amount

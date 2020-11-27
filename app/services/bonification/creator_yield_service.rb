658d0ce93ce35ea1ae4b1a9b76901d57cbbd17cf
vinibr3 <vinicius.deoliveira@outlook.com>
2020-11-27 14:32:28 -0300

    Pay 5% on publicity bonus when user have 4 direct referrals with order greater than or equal to order's contract. Pay 3% when user unqualified.

--- app/services/bonification/creator_yield_service.rb
+++ app/services/bonification/creator_yield_service.rb
@@ -12,8 +12,11 @@ module Bonification
 
     private
 
+    NON_QUALIFIED_USER_RENTABILITY = 0.03
+    VALID_CONTRACTS_MINIMUM_QUANTITY = 4.freeze
+
     def yield_contracts
-      BonusContract.includes(:user, :order)
+      BonusContract.includes(:order, user: [sponsored: [:bonus_contracts]])
                    .active
                    .yield_contracts
     end
@@ -24,9 +27,26 @@ module Bonification
           .create!(spreader: User.find_morenwm_customer_admin,
                    financial_reason: FinancialReason.yield_bonus,
                    moneyflow: :credit,
-                   cent_amount: (contract.cent_amount / 2.0)  * contract.rentability,
+                   cent_amount: (contract.cent_amount / 2.0) * rentability_by_contract(contract),
                    bonus_contract: contract,
                    order: contract.order)
+    end
+
+    def rentability_by_contract(contract)
+      return contract.rentability if user_qualified?(contract)
+
+      NON_QUALIFIED_USER_RENTABILITY
+    end
+
+    def user_qualified?(contract)
+      contract_value = contract.cent_amount
+
+      sponsored_contracts = contract.user
+                                    .sponsored
+                                    .map { |s| s.bonus_contracts }
+                                    .flatten
+      valid_contracts_quantity = sponsored_contracts.count { |c| c.cent_amount >= contract_value }
+      valid_contracts_quantity >= VALID_CONTRACTS_MINIMUM_QUANTITY
     end
 
     def notify_admin_by_email(errors)

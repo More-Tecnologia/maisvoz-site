f42338ff748b77a672800e5eabd1a8a552747a59
vinibr3 <vinicius.deoliveira@outlook.com>
2020-12-09 21:43:05 -0300

    Fix user type qualification by indications quantity and bonus percentage.

--- app/services/financial/payment_compensation.rb
+++ app/services/financial/payment_compensation.rb
@@ -39,6 +39,7 @@ module Financial
         create_vouchers if voucher_product
         create_bonus_contract if deposit_product && enabled_bonification
         create_system_fee if order.products.any?(&:system_taxable) && enabled_bonification
+        update_user_and_sponsor_types
         notify_user_by_email_about_paid_order
       end
     end
@@ -197,6 +198,11 @@ module Financial
 
     def create_bonus_contract
       CreatorBonusContractService.call(order: order, enabled_bonification: enabled_bonification)
+    end
+
+    def update_user_and_sponsor_types
+      Types::QualifierService.call(user: user)
+      Types::QualifierService.call(user: user.sponsor)
     end
   end
 end

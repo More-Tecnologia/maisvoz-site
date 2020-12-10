4750e74d25495b749e18efcf8c1f09811a3cf3a8
vinibr3 <vinicius.deoliveira@outlook.com>
2020-12-10 18:02:51 -0300

    Create bonus contract when disable bonification

--- app/services/financial/payment_compensation.rb
+++ app/services/financial/payment_compensation.rb
@@ -37,7 +37,7 @@ module Financial
         upgrade_career_from(user) if deposit_product
         propagate_bonuses if enabled_bonification
         create_vouchers if voucher_product
-        create_bonus_contract if deposit_product && enabled_bonification
+        create_bonus_contract if deposit_product
         create_system_fee if order.products.any?(&:system_taxable) && enabled_bonification
         update_user_and_sponsor_types
         notify_user_by_email_about_paid_order

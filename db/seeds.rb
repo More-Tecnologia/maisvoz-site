c6aa97f966ad33d9bf255fd7eb87c5c1c5202418
vinibr3 <vinicius.deoliveira@outlook.com>
2021-09-21 17:42:06 -0300

    Fix identation

--- db/seeds.rb
+++ db/seeds.rb
@@ -153,7 +153,6 @@ type_attributes.each do |attributes|
   type ? type.update!(attributes) : Type.create!(type_attributes)
 end
 
-
 # SCORE TYPES
 score_types = [{ name: 'Membership Score', code: '100', active: false },
                { name: 'Activation Score', code: '200', active: false },
@@ -174,7 +173,6 @@ score_types.each do |score_type|
     ScoreType.create!(score_type)
   end
 end
-
 
 # FINANCIAL REASONS
 administrative_type = FinancialReasonType.find_or_create_by!(name: 'Financial administration', code: '100')
@@ -201,44 +199,44 @@ administrative_reasons.each do |attributes|
   end
 end
 
-  bonus_type = FinancialReasonType.find_or_create_by!(name: 'Bonus', code: '200')
-  bonus_reasons = [{ title: 'Bonus chargeback', code: '100', active: false, company_moneyflow: :credit },
-                   { title: 'Binary Bonus', code: '500', active: false, company_moneyflow: :debit   },
-                   { title: 'Binary Bonus Chargeback for Inactivity', code: '600', active: false, company_moneyflow: :credit  },
-                   { title: 'Binary Bonus Chargeback for Monthly Excess', code: '700', active: false, company_moneyflow: :credit  },
-                   { title: 'Weekly Excess Binary Bonus Chargeback', code: '800', active: false, company_moneyflow: :credit  },
-                   { title: 'Career Limit Bonus Chargeback', code: '900', active: false, company_moneyflow: :credit  },
-                   { title: 'Referral Bonus', code: '1000', active: false, company_moneyflow: :debit },
-                   { title: 'Bônus Clickcach', code: '1100', active: true, company_moneyflow: :debit },
-                   { title: 'Binary Bonus Chargeback by Disqualification', code: '1300', active: false, company_moneyflow: :credit },
-                   { title: 'Residual Support Point Bonus', code: '1400', active: false, company_moneyflow: :debit },
-                   { title: 'Residual Support Point Inactivity Bonus Chargeback', code: '3000', active: false, company_moneyflow: :credit},
-                   { title: 'Direct Referral Bonus', code: '2000', active: true, company_moneyflow: :debit  },
-                   { title: 'Chargeback Direct Referral Bonus By Inactivity', code: '2100', active: true, company_moneyflow: :credit },
-                   { title: 'Indirect Referral Bonus', code: '2200', dynamic_compression: true, active: true, company_moneyflow: :debit },
-                   { title: 'Chargeback Indirect Referral Bonus By Inactivity', code: '2300', active: true, company_moneyflow: :credit },
-                   { title: 'Activation Bonus', code: '2400', dynamic_compression: true, active: false, company_moneyflow: :debit },
-                   { title: 'Inactivity Activation Bonus Chargeback', code: '2500', active: false, company_moneyflow: :credit },
-                   { title: 'Residual Bonus', code: '2600', dynamic_compression: false, active: false, company_moneyflow: :debit },
-                   { title: 'Residual Bonus Chargeback for Inactivity', code: '2700', active: false, company_moneyflow: :credit },
-                   { title: 'Support Point Activation Bonus', code: '3100', active: false, company_moneyflow: :debit },
-                   { title: 'Bonus Chargeback Activation of Inactivity Support Point', code: '3200', active: false, company_moneyflow: :credit },
-                   { title: 'Binary Bonus Chargeback for Daily Excess', code: '3300', active: false, company_moneyflow: :credit },
-                   { title: 'Bonus Chargeback for Contract Limit', code: '3400', active: false, company_moneyflow: :credit },
-                   { title: 'Pool Cash', code: '3500', active: false, company_moneyflow: :debit },
-                   { title: 'Equilibrium Bonus', code: '3600', active: false, company_moneyflow: :debit },
-                   { title: 'Equilibrium Bonus Chargeback by Inactivity', code: '3700', active: false, company_moneyflow: :credit },
-                   { title: 'Pool Leadership', code: '3900', active: false, company_moneyflow: :credit },
-                   { title: 'Pool Leadership Chargeback by Inactivity', code: '4000', active: false, company_moneyflow: :credit }]
+bonus_type = FinancialReasonType.find_or_create_by!(name: 'Bonus', code: '200')
+bonus_reasons = [{ title: 'Bonus chargeback', code: '100', active: false, company_moneyflow: :credit },
+                 { title: 'Binary Bonus', code: '500', active: false, company_moneyflow: :debit   },
+                 { title: 'Binary Bonus Chargeback for Inactivity', code: '600', active: false, company_moneyflow: :credit  },
+                 { title: 'Binary Bonus Chargeback for Monthly Excess', code: '700', active: false, company_moneyflow: :credit  },
+                 { title: 'Weekly Excess Binary Bonus Chargeback', code: '800', active: false, company_moneyflow: :credit  },
+                 { title: 'Career Limit Bonus Chargeback', code: '900', active: false, company_moneyflow: :credit  },
+                 { title: 'Referral Bonus', code: '1000', active: false, company_moneyflow: :debit },
+                 { title: 'Bônus Clickcach', code: '1100', active: true, company_moneyflow: :debit },
+                 { title: 'Binary Bonus Chargeback by Disqualification', code: '1300', active: false, company_moneyflow: :credit },
+                 { title: 'Residual Support Point Bonus', code: '1400', active: false, company_moneyflow: :debit },
+                 { title: 'Residual Support Point Inactivity Bonus Chargeback', code: '3000', active: false, company_moneyflow: :credit},
+                 { title: 'Direct Referral Bonus', code: '2000', active: true, company_moneyflow: :debit  },
+                 { title: 'Chargeback Direct Referral Bonus By Inactivity', code: '2100', active: true, company_moneyflow: :credit },
+                 { title: 'Indirect Referral Bonus', code: '2200', dynamic_compression: true, active: true, company_moneyflow: :debit },
+                 { title: 'Chargeback Indirect Referral Bonus By Inactivity', code: '2300', active: true, company_moneyflow: :credit },
+                 { title: 'Activation Bonus', code: '2400', dynamic_compression: true, active: false, company_moneyflow: :debit },
+                 { title: 'Inactivity Activation Bonus Chargeback', code: '2500', active: false, company_moneyflow: :credit },
+                 { title: 'Residual Bonus', code: '2600', dynamic_compression: false, active: false, company_moneyflow: :debit },
+                 { title: 'Residual Bonus Chargeback for Inactivity', code: '2700', active: false, company_moneyflow: :credit },
+                 { title: 'Support Point Activation Bonus', code: '3100', active: false, company_moneyflow: :debit },
+                 { title: 'Bonus Chargeback Activation of Inactivity Support Point', code: '3200', active: false, company_moneyflow: :credit },
+                 { title: 'Binary Bonus Chargeback for Daily Excess', code: '3300', active: false, company_moneyflow: :credit },
+                 { title: 'Bonus Chargeback for Contract Limit', code: '3400', active: false, company_moneyflow: :credit },
+                 { title: 'Pool Cash', code: '3500', active: false, company_moneyflow: :debit },
+                 { title: 'Equilibrium Bonus', code: '3600', active: false, company_moneyflow: :debit },
+                 { title: 'Equilibrium Bonus Chargeback by Inactivity', code: '3700', active: false, company_moneyflow: :credit },
+                 { title: 'Pool Leadership', code: '3900', active: false, company_moneyflow: :credit },
+                 { title: 'Pool Leadership Chargeback by Inactivity', code: '4000', active: false, company_moneyflow: :credit }]
 
-  bonus_reasons.each do |attributes|
-    financial_reason = FinancialReason.find_by(code: attributes[:code])
-    if financial_reason
-      financial_reason.update_attributes(attributes)
-    else
-      FinancialReason.find_or_create_by!(attributes.merge({financial_reason_type: bonus_type}))
-    end
+bonus_reasons.each do |attributes|
+  financial_reason = FinancialReason.find_by(code: attributes[:code])
+  if financial_reason
+    financial_reason.update_attributes(attributes)
+  else
+    FinancialReason.find_or_create_by!(attributes.merge({financial_reason_type: bonus_type}))
   end
+end
 
   # USERS
   more_user = User.new(username: ENV['MORENWM_USERNAME'],

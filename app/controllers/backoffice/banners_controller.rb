5de5513e4d4e7ac67845f2c357839a85156561c6
heirian <26394609+Heirian@users.noreply.github.com>
2021-10-05 10:31:51 -0300

    Removes deprecated show daily task alert callback

--- app/controllers/backoffice/banners_controller.rb
+++ app/controllers/backoffice/banners_controller.rb
@@ -1,9 +1,5 @@
 module Backoffice
   class BannersController < BackofficeController
-    before_action :show_daily_task_alert, only: :index,
-                                          unless: proc { current_user.banner_seen_today? },
-                                          if: proc { current_user.bonus_contracts.active.any? }
-
     def index
       @banners = current_user.banner_clicks
                              .today

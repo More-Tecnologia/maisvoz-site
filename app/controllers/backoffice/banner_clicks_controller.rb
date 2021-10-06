937e618fc8a45fbe1e37ac62fdd5d6205c82db88
heirian <26394609+Heirian@users.noreply.github.com>
2021-10-06 14:53:29 -0300

    Updates banner click flow logic

--- app/controllers/backoffice/banner_clicks_controller.rb
+++ app/controllers/backoffice/banner_clicks_controller.rb
@@ -5,21 +5,37 @@ module Backoffice
 
     def create
       ActiveRecord::Base.transaction do
-        @banner = Banner.find(params[:banner_id])
-        if current_user.can_click_more_banners?
-          @banner_click = current_user.banner_clicks
-                                      .create!(params.slice(:banner_id))
+        bonus_contracts.each do |bonus_contract|
+          banner_click = current_user.banner_clicks
+                                     .create!(params.slice(:banner_id))
+          transaction = build_transaction(bonus_contract)
+          credit_bonus_to(bonus_contract)
 
-          transaction = current_user.financial_transactions
-                                    .create!(spreader: @user,
-                                             financial_reason: FinancialReason.yield_bonus,
-                                             moneyflow: :credit,
-                                             cent_amount: current_user.current_earning)
-          @banner_click.update(financial_transaction: transaction)
-          RecurrentCreatorWorker.perform_async(current_user.id)
-          current_user.banner_seen_today! if current_user.viewed_minimum_banner_quantity_today?
+          banner_click.update(financial_transaction: transaction)
+          RecurrentCreatorWorker.perform_async(current_user.id, transaction.id)
         end
       end
+    end
+
+    def bonus_contracts
+      current_user.bonus_contracts.active.yield_contracts.order(:created_at)
+    end
+
+    def build_transaction(contract)
+      current_user.financial_transactions
+                  .create!(spreader: User.find_morenwm_customer_admin,
+                           financial_reason: FinancialReason.yield_bonus,
+                           moneyflow: :credit,
+                           cent_amount: contract.order_items
+                                                .last
+                                                .earnings_per_campaign
+                                                .to_f / 100.0)
+    end
+
+    def credit_bonus_to(contract)
+      contract.received_balance += bonus_amount.to_f
+      contract.remaining_balance = contract.cent_amount - contract.received_balance.to_f
+      contract.save!
     end
   end
 end

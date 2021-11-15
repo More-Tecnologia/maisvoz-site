687e941541c054ff4cf22605c6ed1b8a3850c1c0
vinibr3 <vinicius.deoliveira@outlook.com>
2021-11-14 21:32:39 -0300

    Block duplicated purchase of free product

--- app/controllers/backoffice/carts_controller.rb
+++ app/controllers/backoffice/carts_controller.rb
@@ -1,5 +1,6 @@
 module Backoffice
   class CartsController < BackofficeController
+    before_action :redirect_back_if_duplicated_bought_of_free_product
 
     def show
       @checkout_form = CheckoutForm.new(order: current_order, user: current_user)
@@ -34,6 +35,16 @@ module Backoffice
     def valid_params
       params.permit(:payment_method)
             .merge(order: current_order)
+    end
+
+    def redirect_back_if_duplicated_bought_of_free_product
+      return if OrderItem.includes(:product, :order)
+                         .where(order: Order.paid, 'orders.user': current_user)
+                         .where(product: Product.first)
+                         .none?
+
+      flash[:alert] = t('defaults.errors.duplicated_free_product')
+      redirect_back(fallback_location: backoffice_dashboard_index_path)
     end
   end
 end

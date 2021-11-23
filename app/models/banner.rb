39e059c610f932fc7562a86c1ad64968e897246c
heirian <26394609+Heirian@users.noreply.github.com>
2021-11-23 18:23:39 -0300

    Updates banner images links

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -2,7 +2,6 @@ class Banner < ApplicationRecord
   has_attachment :image
 
   validates :link, presence: true
-  validates :image_path, presence: true
 
   scope :active, -> { where(active: true) }
 

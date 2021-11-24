a1503ff4ca5ba2571135513f6a45adeb07a88035
heirian <26394609+Heirian@users.noreply.github.com>
2021-11-24 19:30:21 -0300

    bugfix

--- app/models/system_configuration.rb
+++ app/models/system_configuration.rb
@@ -6,21 +6,18 @@ class SystemConfiguration < ApplicationRecord
   scope :active, -> { where(active: true) }
 
   def self.active_config
-    # active.first
+    active.first
   end
 
   def self.company_name
-    # active_config.try(:company_name).presence ||
-    ENV['COMPANY_NAME']
+    active_config.try(:company_name).presence || ENV['COMPANY_NAME']
   end
 
   def self.taxable_fee
-    # active_config.try(:taxable_fee).presence ||
-    ENV['SYSTEM_FEE']
+    active_config.try(:taxable_fee).presence || ENV['SYSTEM_FEE']
   end
 
   def self.withdrawal_fee
-    # active_config.try(:withdrawal_fee).presence ||
-    ENV['WITHDRAWAL_FEE']
+    active_config.try(:withdrawal_fee).presence || ENV['WITHDRAWAL_FEE']
   end
 end

629ea55311fdecd65f5c7bde47100083f80b1a4f
Ítalo Braga <italojbraga@gmail.com>
2021-11-26 10:59:35 -0300

    Add changes in admin layout

--- app/assets/javascripts/click-tok-theme/box-selected.js
+++ app/assets/javascripts/click-tok-theme/box-selected.js
@@ -1,17 +1,4 @@
-let item1 = document.querySelector(".item1");
-let item2 = document.querySelector(".item2");
-let item3 = document.querySelector(".item3");
-let item4 = document.querySelector(".item4");
-let item5 = document.querySelector(".item5");
-let item6 = document.querySelector(".item6");
-let item7 = document.querySelector(".item7");
-let item8 = document.querySelector(".item8");
-let item9 = document.querySelector(".item9");
-let item10 = document.querySelector(".item10");
-let item11 = document.querySelector(".item11");
-let item12 = document.querySelector(".item12");
-
-let items = [item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12];
+let items = document.querySelectorAll('.card-menu-admin');
 
 function boxSelected(){
     items.forEach((item) => {

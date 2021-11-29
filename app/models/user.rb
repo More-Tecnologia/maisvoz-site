668b2f5b1b054122e97e0e0180137396339acd10
heirian <26394609+Heirian@users.noreply.github.com>
2021-11-29 15:23:32 -0300

    Adds confirmable to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -109,7 +109,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable,
          :trackable, :validatable, :lockable, :masqueradable
 
   has_attachment :avatar, accept: [:jpg, :png]

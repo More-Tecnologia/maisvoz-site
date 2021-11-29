d290b8b4272e96d95af5e95b1bbbe3538afe3214
heirian <26394609+Heirian@users.noreply.github.com>
2021-11-29 17:04:17 -0300

    Updates financial transactions admin layout

--- app/controllers/backoffice/financial_transactions_controller.rb
+++ app/controllers/backoffice/financial_transactions_controller.rb
@@ -3,13 +3,18 @@ module Backoffice
     include Backoffice::FinancialTransactionsHelper
 
     def index
-      @financial_transactions =
-        FinancialTransaction.by_current_user(current_user)
-                            .to_empreendedor
-                            .includes(:spreader, :financial_reason, :order)
-                            .order(created_at: :desc)
-                            .page(params[:page])
-                            .per(10)
+      if current_user.empreendedor?
+        @financial_transactions =
+          FinancialTransaction.by_current_user(current_user)
+                              .to_empreendedor
+                              .includes(:spreader, :financial_reason, :order)
+                              .order(created_at: :desc)
+                              .page(params[:page])
+                              .per(10)
+      else
+        @q = FinancialTransaction.ransack(params[:q])
+      @financial_transactions = find_financial_transactions_by_current_user(@q)
+      end
     end
   end
 end

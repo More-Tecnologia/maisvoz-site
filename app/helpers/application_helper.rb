a312a1c535b0ca6d6812060c9c4dbbe7615d6829
heirian <26394609+Heirian@users.noreply.github.com>
2021-11-30 17:25:42 -0300

    Adds order expire warning text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -89,4 +89,8 @@ module ApplicationHelper
       content_tag :i, '', class: 'fas fa-copy text-white'
     end
   end
+
+  def seconds_to_hms(sec)
+    "%02d:%02d:%02d" % [sec / 3600, sec / 60 % 60, sec % 60]
+  end
 end

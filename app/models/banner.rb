0dfcf676954fd68651022cdb9543f013b6879005
heirian <26394609+Heirian@users.noreply.github.com>
2021-12-02 19:37:49 -0300

    Adds premium to banner

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -6,6 +6,8 @@ class Banner < ApplicationRecord
   validates :link, presence: true
 
   scope :active, -> { where(active: true) }
+  scope :premium, -> { where(premium: true) }
+  scope :default, -> { where(premium: false) }
 
   def path
     image.try(:fullpath)

4b93b53266c3d0507eff0180fe83cc86598990ec
heirian <26394609+Heirian@users.noreply.github.com>
2021-12-06 17:09:26 -0300

    bugfix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require rails-ujs
 //= require 'bootstrap'
 //= require 'jquery.webui-popover'
 //= require owl.carousel.min

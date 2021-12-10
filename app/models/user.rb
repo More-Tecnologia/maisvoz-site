76e9628b0c77dcbfa7d0f5581a82c999964e816a
heirian <26394609+Heirian@users.noreply.github.com>
2021-12-10 15:44:54 -0300

    bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -110,7 +110,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-         :trackable, :validatable, :lockable, :masqueradable, :timeoutable
+         :trackable, :validatable, :lockable, :timeoutable, :masqueradable, :confirmable
 
   has_attachment :avatar, accept: [:jpg, :png]
   has_attachment :document_rg_photo

61f80f441e42e1c81a58156a93890b40d482ece2
Ítalo Braga <italojbraga@gmail.com>
2021-12-17 12:28:45 -0300

    Update footer.

--- app/assets/javascripts/click-tok-theme/footer.js
+++ app/assets/javascripts/click-tok-theme/footer.js
@@ -19,52 +19,98 @@ function moveMenu() {
   let x = 0;
 
   list.forEach((item, i) => {
-      if(item.className == "list active"){
-        indicator.style.transition = "0.5s"
-          indicator.style.transform = `translateX(calc(${item.offsetWidth+"px"} * ${x}))`;
-      }
-      x++;
+    if (item.className == "list active") {
+      indicator.style.transition = "0.5s"
+      indicator.style.transform = `translateX(calc(${item.offsetWidth+"px"} * ${x}))`;
+    }
+    x++;
   })
 }
 
-function itemSelected(){
+function itemSelected() {
   let links = document.querySelectorAll('.link-nav');
   let sizeNav = document.querySelector('.list').offsetWidth;
   let firstList = list[0];
-  list.forEach((item) => {
+  
+  let footerDashboardHome = ["/",
+    "/backoffice/admin/dashboard",
+    "/backoffice/home"
+  ];
 
-      if(window.location.pathname == "/backoffice/dashboard" || window.location.pathname == "/backoffice/dashboard"){
-        indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 1))`;
-        indicator.style.transition = "none";
-      }
-      if(window.location.pathname == "/backoffice/support/users" || window.location.pathname == "/backoffice/dashboard"){
-        indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 1))`;
-        indicator.style.transition = "none";
-      }
-      if(window.location.pathname == "/backoffice/products"){
-        indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 2))`;
-        indicator.style.transition = "none";
-      }
-      if(window.location.pathname == "/backoffice/financial_dashboard"){
-        indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 3))`;
-        indicator.style.transition = "none";
-      }
-      if(window.location.pathname == "/backoffice/team_dashboard"){
-          indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 4))`;
-          indicator.style.transition = "none";
-        }
-      if(item.children[0].pathname == window.location.pathname){
-        item.classList.add('active');
-      }
-      else {
-        item.classList.remove('active');
-      }
-      if(window.location.pathname !== "/backoffice/support/users" && window.location.pathname !== "/backoffice/products" && window.location.pathname !== "/backoffice/financial_dashboard" && window.location.pathname !== "/backoffice/team_dashboard" && window.location.pathname !== "/backoffice/dashboard"){
-        firstList.classList.add('active')
-        indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 0))`;
-        indicator.style.transition = "none";
-      }
-  });
+  let footerTasksUsers = ["/backoffice/dashboard",
+    "/backoffice/banners",
+    "/backoffice/support/users"
+  ];
+
+  let footerShop = ["/backoffice/products",
+    "/backoffice/stores/games",
+    "/backoffice/stores/courses",
+    "/backoffice/stores/ads",
+    "/backoffice/cart"
+  ];
+
+  let footerFinancial = ["/backoffice/financial_dashboard",
+    "/backoffice/withdrawals",
+    "/backoffice/financial_transactions",
+    "/backoffice/orders",
+    "/backoffice/bank_account/edit.1",
+    "/backoffice/withdrawals/new",
+    "/backoffice/admin/withdrawals",
+    "/backoffice/admin/credits_debits/find_user",
+    "/backoffice/admin/expenses/new",
+    "/backoffice/admin/orders",
+    "/backoffice/admin/financial_transactions",
+    "/backoffice/admin/bonus_contracts"
+  ];
+
+  let footerTeam = ["/backoffice/team_dashboard"]
+
+  if (footerDashboardHome.includes(window.location.pathname)) {
+    indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 0))`;
+    indicator.style.transition = "none";
+    list[0].classList.add('active');
+  } else {
+    list[0].classList.remove('active');
+  }
+  if (footerTasksUsers.includes(window.location.pathname)) {
+    indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 1))`;
+    indicator.style.transition = "none";
+    list[1].classList.add('active');
+  } else {
+    list[1].classList.remove('active');
+  }
+  if (footerShop.includes(window.location.pathname)) {
+    indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 2))`;
+    indicator.style.transition = "none";
+    list[2].classList.add('active');
+  } else {
+    list[2].classList.remove('active');
+  }
+  if (footerFinancial.includes(window.location.pathname)) {
+    indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 3))`;
+    indicator.style.transition = "none";
+    list[3].classList.add('active');
+  } else {
+    list[3].classList.remove('active');
+  }
+  if (footerTeam.includes(window.location.pathname)) {
+    indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 4))`;
+    indicator.style.transition = "none";
+    list[4].classList.add('active');
+  } else {
+    list[4].classList.remove('active');
+  }
+
+  if (footerDashboardHome.includes(window.location.pathname) == false &&
+    footerTasksUsers.includes(window.location.pathname) == false &&
+    footerShop.includes(window.location.pathname) == false &&
+    footerFinancial.includes(window.location.pathname) == false &&
+    footerTeam.includes(window.location.pathname) == false) {
+
+    firstList.classList.add('active')
+    indicator.style.transform = `translateX(calc(${sizeNav+"px"} * 0))`;
+    indicator.style.transition = "none";
+  }
 }
 
 itemSelected();

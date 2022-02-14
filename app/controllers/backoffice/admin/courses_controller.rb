9383512a51bdf1cb82f6791078ffec596d7b98af
heirian <26394609+Heirian@users.noreply.github.com>
2022-02-14 17:27:57 -0300

    Adds courses admin menu

--- app/controllers/backoffice/admin/courses_controller.rb
+++ app/controllers/backoffice/admin/courses_controller.rb
@@ -3,6 +3,13 @@
 module Backoffice
   module Admin
     class CoursesController < AdminController
+      def index
+        @q = Course.ransack(params)
+        @courses = @q.result
+                     .order(created_at: :desc)
+                     .page(params[:page])
+      end
+
       def update
       end
 

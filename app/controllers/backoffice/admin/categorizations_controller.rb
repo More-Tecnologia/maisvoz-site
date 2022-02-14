b419bb7741cbf6ccc0672a30ae76dbcf61e7d62d
heirian <26394609+Heirian@users.noreply.github.com>
2022-02-14 16:02:00 -0300

    routes fix

--- app/controllers/backoffice/admin/categorizations_controller.rb
+++ app/controllers/backoffice/admin/categorizations_controller.rb
@@ -22,7 +22,7 @@ module Backoffice
         @categorization = Categorization.new(ensured_params)
         if @categorization.save!
           flash[:success] = I18n.t('defaults.success')
-          redirect_to @categorization
+          redirect_to [:backoffice, :admin, @categorization]
         else
           flash[:error] = @categorization.errors.map(&:message)
           render 'new'
@@ -34,7 +34,7 @@ module Backoffice
       def update
         if @categorization.update!(ensured_params)
           flash[:success] = I18n.t('defaults.success')
-          redirect_to @categorization
+          redirect_to [:backoffice, :admin, @categorization]
         else
           flash[:error] = @categorization.errors.map(&:message)
           render 'edit'

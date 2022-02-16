0fe181edc502b1afa593bd3ee714751aa6a35049
heirian <26394609+Heirian@users.noreply.github.com>
2022-02-16 17:00:02 -0300

    Adds enum helper

---
+++ app/helpers/enum_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module EnumHelper
+  def options_for_enum(object, enum)
+    options = enums_to_translated_options_array(object.class.name, enum.to_s)
+    options_for_select(options, object.send(enum))
+  end
+
+  def enums_to_translated_options_array(klass, enum)
+    klass.classify.safe_constantize.send(enum.pluralize).map do |key, _value|
+      [I18n.t("activerecord.enums.#{klass.downcase}.#{enum}.#{key}").humanize, key]
+    end
+  end
+end

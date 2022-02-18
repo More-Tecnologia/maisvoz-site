c1c97c261958b0f7bf71b85563f6814f1c19bcc7
heirian <26394609+Heirian@users.noreply.github.com>
2022-02-18 10:46:19 -0300

    Removes unused method on admin dashboard presenter

--- app/presenters/dashboards/admins/index_presenter.rb
+++ app/presenters/dashboards/admins/index_presenter.rb
@@ -3,58 +3,12 @@
 module Dashboards
   module Admins
     class IndexPresenter
-
-      def active_products_count
-        Product.active.count
+      def answered_tickets_count
+        Ticket.select(:status).answered.count
       end
 
-      def data_list
-        [
-          { label: I18n.t('views.dashboard.admin.today_deposits'),
-            value: today_orders_count,
-            icon: 'md-attach-money'
-          },
-          { label: I18n.t('views.dashboard.admin.total_deposits'),
-            value: orders_count,
-            icon: 'md-add-shopping-cart'
-          },
-          { label: I18n.t('views.dashboard.admin.active_products'),
-            value: active_products_count,
-            icon: 'md-store-mall-directory'
-          },
-          { label: I18n.t('views.dashboard.admin.total_users'),
-            value: users_count,
-            icon: 'md-account-child'
-          }
-        ]
-      end
-
-      def last_orders
-        Order.includes(:user).valids.last(10).reverse
-      end
-
-      def last_orders_count
-        Order.created_after(10)
-             .group_by_day(:created_at)
-             .count
-      end
-
-      def last_users_count
-        User.created_after(10)
-            .group_by_day(:created_at)
-            .count
-      end
-
-      def orders_count
-        Order.valids.count
-      end
-
-      def today_orders_count
-        Order.valids.today.count
-      end
-
-      def users_count
-        User.count
+      def finished_tickets_count
+        Ticket.select(:status).finished.count
       end
 
       def waiting_tickets
@@ -63,14 +17,6 @@ module Dashboards
 
       def waiting_tickets_count
         Ticket.select(:status).waiting.count
-      end
-
-      def answered_tickets_count
-        Ticket.select(:status).answered.count
-      end
-
-      def finished_tickets_count
-        Ticket.select(:status).finished.count
       end
     end
   end

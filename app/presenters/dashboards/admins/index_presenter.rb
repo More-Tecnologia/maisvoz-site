5a82940c1aef769883d033390634274571d9ec8b
heirian <26394609+Heirian@users.noreply.github.com>
2022-02-21 19:29:10 -0300

    Refactor admin index presenter

--- app/presenters/dashboards/admins/index_presenter.rb
+++ app/presenters/dashboards/admins/index_presenter.rb
@@ -7,56 +7,20 @@ module Dashboards
         Ticket.select(:status).answered.count
       end
 
-      def finished_tickets_count
-        Ticket.select(:status).finished.count
+      def admin_order_count
+        Order.paid.admin.count
       end
 
-      def waiting_tickets
-        Ticket.waiting
+      def admin_nb_order_count
+        Order.paid.admin_nb.count
       end
 
-      def waiting_tickets_count
-        Ticket.select(:status).waiting.count
+      def btc_order_count
+        Order.paid.btc.count
       end
 
-      def total_balance
-        system_balance + pool_balance
-      end
-
-      def total_balance_dollar
-        to_btc(total_balance)
-      end
-
-      def pool_balance
-        @pool_balance ||= PoolWallet.sum(&:cent_amount)
-      end
-
-      def pool_balance_dollar
-        to_btc(pool_balance)
-      end
-
-      def system_balance
-        incoming_balance - paid_withdraws_balance - expenses_balance
-      end
-
-      def system_balance_dollar
-        to_btc(system_balance)
-      end
-
-      def incoming_balance
-        system_balance_orders.sum(&:amount)
-      end
-
-      def virtual_balance
-        User.find_morenwm_customer_admin.available_balance
-      end
-
-      def virtual_balance_btc
-        User.find_morenwm_customer_admin.available_balance * btc_rate
-      end
-
-      def expenses_balance
-        FinancialTransaction.where(financial_reason: FinancialReason.expense).sum(&:cent_amount) * btc_rate
+      def canceled_withdraws_count
+        Withdrawal.canceled.count
       end
 
       def courses_balance
@@ -67,12 +31,60 @@ module Dashboards
         to_btc(courses_balance)
       end
 
+      def crypto_balance
+        OrderItem.includes(:product, order: :payment_transaction).where(product: Product.crypto, order: Order.paid.currency_balance).sum { |oi| oi.order.payment_transaction.amount }
+      end
+
+      def crypto_balance_dollar
+        to_btc(crypto_balance)
+      end
+
+      def expenses_balance
+        FinancialTransaction.where(financial_reason: FinancialReason.expense).sum(&:cent_amount) * btc_rate
+      end
+
+      def finished_tickets_count
+        Ticket.select(:status).finished.count
+      end
+
+      def free_order_count
+        Order.paid.free.count
+      end
+
+      def pool_balance
+        @pool_balance ||= PoolWallet.sum(&:cent_amount)
+      end
+
+      def pool_balance_dollar
+        to_btc(pool_balance)
+      end
+
+      def incoming_balance
+        system_balance_orders.sum(&:amount)
+      end
+
       def packages_balance
         OrderItem.includes(:product, order: :payment_transaction).where(product: Product.deposit, order: Order.paid.currency_balance).sum { |oi| oi.order.payment_transaction.amount }
       end
 
       def packages_balance_dollar
         to_btc(packages_balance)
+      end
+
+      def paid_withdraws_count
+        Withdrawal.approved.count
+      end
+
+      def paid_withdraws_balance
+        Withdrawal.select(:crypto_amount).approved.sum(&:crypto_amount)
+      end
+
+      def pending_withdraws_count
+        Withdrawal.pending.count
+      end
+
+      def pool_wallets
+        PoolWallet.all
       end
 
       def publicity_balance
@@ -83,74 +95,62 @@ module Dashboards
         to_btc(publicity_balance)
       end
 
-      def crypto_balance
-        OrderItem.includes(:product, order: :payment_transaction).where(product: Product.crypto, order: Order.paid.currency_balance).sum { |oi| oi.order.payment_transaction.amount }
-      end
-
-      def crypto_balance_dollar
-        to_btc(crypto_balance)
-      end
-
-      def free_order_count
-        Order.paid.free.count
-      end
-
-      def btc_order_count
-        Order.paid.btc.count
-      end
-
-      def admin_order_count
-        Order.paid.admin.count
-      end
-
-      def admin_nb_order_count
-        Order.paid.admin_nb.count
-      end
-
-      def paid_withdraws_count
-        Withdrawal.approved.count
-      end
-
-      def pending_withdraws_count
-        Withdrawal.pending.count
-      end
-
-      def waiting_withdraws_count
-        Withdrawal.waiting.count
-      end
-
       def rejected_withdraws_count
         Withdrawal.refused.count
       end
 
-      def canceled_withdraws_count
-        Withdrawal.canceled.count
+      def system_balance
+        incoming_balance - paid_withdraws_balance - expenses_balance
       end
 
-      def paid_withdraws_balance
-        Withdrawal.select(:crypto_amount).approved.sum(&:crypto_amount)
-      end
-
-      def pool_wallets
-        PoolWallet.all
+      def system_balance_dollar
+        to_btc(system_balance)
       end
 
       def to_btc(amount)
         amount / btc_rate
       end
 
+      def total_balance
+        system_balance + pool_balance
+      end
+
+      def total_balance_dollar
+        to_btc(total_balance)
+      end
+
+      def virtual_balance
+        User.find_morenwm_customer_admin.available_balance
+      end
+
+      def virtual_balance_btc
+        User.find_morenwm_customer_admin.available_balance * btc_rate
+      end
+
+      def waiting_withdraws_count
+        Withdrawal.waiting.count
+      end
+
+      def waiting_tickets
+        Ticket.waiting
+      end
+
+      def waiting_tickets_count
+        Ticket.select(:status).waiting.count
+      end
+
       private
 
-      def system_balance_orders
-        PaymentTransaction.select(:amount).where(order: Order.paid.currency_balance)
+      def btc_rate
+        rates[:BTC]
       end
 
       def rates
         @rates ||= Webhooks::Coinbase::DollarExchangeRates.call
       end
 
-      def btc_rate
-        rates[:BTC]
+      def system_balance_orders
+        PaymentTransaction.select(:amount).where(order: Order.paid.currency_balance)
       end
     end
   end

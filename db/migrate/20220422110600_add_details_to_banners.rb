ad5ed661fb6d3f1ea52c29e451524de17e16d115
sk8marlon <marlonsgb@hotmail.com>
2022-03-22 12:56:11 -0300

    requested changes

--- db/migrate/20220422110600_add_details_to_banners.rb
+++ db/migrate/20220422110600_add_details_to_banners.rb
@@ -1,11 +1,11 @@
 class AddDetailsToBanners < ActiveRecord::Migration[5.2]
-    def change
-      add_column :banners, :country_of_operation, :string, array: true,  default: []
-      add_column :banners, :title, :string
-      add_column :banners, :description, :string
-      add_column :banners, :status, :string
-      add_column :banners, :views, :integer
-      add_column :banners, :current_clicks, :integer
-    end
+  def change
+    add_column :banners, :country_of_operation, :string, array: true,  default: [], null: false
+    add_column :banners, :title, :string, default: '', null: false
+    add_column :banners, :description, :text, default: '', null: false
+    add_column :banners, :status, :integer, default: 0
+    add_column :banners, :views, :integer, default: 0
+    add_column :banners, :current_clicks, :integer, default: 0
   end
+end
   

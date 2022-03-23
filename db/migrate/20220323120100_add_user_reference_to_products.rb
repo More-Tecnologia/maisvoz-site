2ac2ef792b8cbc5977c3d048ca10166c144f33c4
kenidy <kenidys@gmailcom>
2022-03-23 12:25:08 -0300

    Add product reference to banners

--- db/migrate/20220323120100_add_user_reference_to_products.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddUserReferenceToProducts < ActiveRecord::Migration[5.2]
-    def change
-      add_reference :products, :user, foreign_key: true      
-    end
-  end

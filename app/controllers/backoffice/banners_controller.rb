8b19c1b34e16b931c91745654590f1b5548d86be
Heirian <26394609+Heirian@users.noreply.github.com>
2022-03-25 03:44:13 -0300

    Updates banners creation flow

--- app/controllers/backoffice/banners_controller.rb
+++ app/controllers/backoffice/banners_controller.rb
@@ -11,30 +11,11 @@ module Backoffice
     end
 
     def new
-      @banner = Banner.new
+      @product = Product.find(params[:product_id])
+      @banner = @product.ads.build
     end
 
     def create
-      flash[:success] = I18n.t(:success_create_banner)
-      redirect_to backoffice_banners_path
-    end
-
-    def update
-      if @banner.update(ensured_params)
-        flash[:success] = I18n.t(:success_update_banner)
-        redirect_to backoffice_banners_path
-      else
-        flash[:error] = @banner.errors.full_messages.join(', ')
-        render :edit
-      end
-    end
-
-    def destroy
-      if @banner.update(active: false)
-        flash[:success] = I18n.t(:success_inactivate_banner)
-      else
-        flash[:error] = @banner.errors.full_messages.join(', ')
-      end
       redirect_to backoffice_banners_path
     end
 
@@ -44,27 +25,40 @@ module Backoffice
       # this step is necessary because of attachinary gem bug -
       # https://github.com/assembler/attachinary/issues/130
       # Remove this gem in favor of active storage
+      @product = Product.find(params[:product_id])
       new_params = ensured_params
       file = new_params.delete(:image)
 
-      @banner = BannerStore.ads.banners.build(new_params)
-      @banner.save
+      @banner = @product.ads.build(new_params)
+      ActiveRecord::Base.transaction do
+        command = Shopping::AddToCart.call(current_ads_cart, @product.id, params[:country])
+        raise StandardError.new(command.errors) unless command.success?
+        @banner.order_item = current_ads_cart.order_items.last
+        if @banner.valid?(:ads)
+          @banner.save
+        end
+      end
       return if @banner.persisted? && @banner.update(image: file)
 
       flash[:error] = @banner.errors.full_messages.join(', ')
       @banner.destroy
       render :new
-    end
-
-    def ensured_banner
-      @banner = Banner.find(params[:id])
+    rescue StandardError => error
+      flash[:error] = error.message
+      render :new
     end
 
     def ensured_params
       params.require(:banner)
-            .permit(:link, :image_path, :image, :title,
+            .permit(:link, :image, :title,
                     :country_of_operation, :description)
-            .merge(user: current_user, premium: true)
+            .merge(user: current_user,
+                   banner_store: BannerStore.ads_store,
+                   premium: true,
+                   active: false,
+                   status: :pendent,
+                   order: current_ads_cart,
+                   current_clicks: @product.clicks)
     end
   end
 end

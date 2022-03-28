234798946d7454ce79ed8ac8627968827c2eb2c0
kenidy <kenidys@gmailcom>
2022-03-28 16:18:43 -0300

    Fix identation

--- config/routes.rb
+++ config/routes.rb
@@ -231,9 +231,7 @@ Rails.application.routes.draw do
     end
 
     resources :raffles_checkout, only: :update
-
     resources :raffles_cart, only: :show
-
     resources :raffles_tickets, only: :index
   end
 

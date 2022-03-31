7951bfc64dc38e7c0c315dc658d7898ef71d4b10
heirian <26394609+Heirian@users.noreply.github.com>
2022-03-31 19:54:50 -0300

    Minor fixes on raffle model

--- app/models/raffle.rb
+++ app/models/raffle.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Raffle < ApplicationRecord
-  enum kind: { started: 0, awaiting_draw: 1, pending: 2, finished: 3 }
+  enum kind: { automatic: 0, manual: 1, flex: 2 }
+  enum status: { started: 0, awaiting_draw_date: 1, awaiting_draw: 2,
+                 pending: 3, finished: 4 }
 
   belongs_to :product
   belongs_to :winner, class_name: 'User', optional: true
@@ -9,13 +11,19 @@ class Raffle < ApplicationRecord
 
   has_many :raffle_tickets
 
-  validates :draw_date, presence: true, on: :set_draw_date
-  validates :lotto_numbers, presence: true, on: :draw
-  validates :lotto_numbers_combination, presence: true, on: :draw
+  validates :draw_date, presence: true, if: :awaiting_draw?
+  validates :lotto_numbers, presence: true, if: :drawn?
+  validates :lotto_numbers_combination, presence: true, if: :drawn?
   validates :max_ticket_number, numericality: { greater_than: 0 }
   validates :title, presence: true
-  validates :winner, presence: true, on: :draw
-  validates :winning_ticket, presence: true, on: :draw
+  validates :winner, presence: true, if: :drawn?
+  validates :winning_ticket, presence: true, if: :drawn?
 
   delegate :price_cents, to: :product
+
+  private
+
+  def drawn?
+    pending? || finished?
+  end
 end

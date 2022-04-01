b34ad4689e36274f96557331052aea8627a60853
heirian <26394609+Heirian@users.noreply.github.com>
2022-04-01 19:49:07 -0300

    Add register and update raffles flow

--- app/controllers/backoffice/admin/raffles_controller.rb
+++ app/controllers/backoffice/admin/raffles_controller.rb
@@ -3,9 +3,61 @@
 module Backoffice
   module Admin
     class RafflesController < AdminController
+      before_action :ensure_raffle, only: %i[edit update]
+
       def index; end
-      
-      def new; end
+
+      def new
+        @raffle = Raffle.new
+      end
+
+      def create
+        build_raffle
+        flash[:success] = t(:success_create)
+        redirect_to backoffice_admin_raffles_path
+      rescue StandardError => e
+        flash[:error] = e.message
+        redirect_back(fallback_location: '')
+      end
+
+      def edit; end
+
+      def update
+        update_raffle
+        flash[:success] = t(:success_updated)
+        redirect_to backoffice_admin_raffles_path
+      rescue StandardError => e
+        flash[:error] = e.message
+        render :edit
+      end
+
+      private
+
+      def build_raffle
+        Raffles::CreateService.call(raffle_params: valid_raffle_params,
+                                    product_params: valid_product_params)
+      end
+
+      def ensure_raffle
+        @raffle = Raffle.find(params[:id])
+      end
+
+      def update_raffle
+        Raffles::UpdateService.call(raffle: @raffle,
+                                    raffle_params: valid_raffle_params,
+                                    product_params: valid_product_params)
+      end
+
+      def valid_product_params
+        params.require(:product)
+              .permit(:short_description, :description, :price_cents)
+              .merge(name: valid_raffle_params[:title])
+      end
+
+      def valid_raffle_params
+        params.require(:raffle)
+              .permit(:title, :max_ticket_number, :kind, :thumb)
+      end
     end
   end
 end
---
+++ app/services/raffles/create_service.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Raffles
+  class CreateService < ApplicationService
+    def initialize(params)
+      @raffle_params = params[:raffle_params]
+      @product_params = params[:product_params]
+      @thumb = @raffle_params.delete(:thumb)
+    end
+
+    private
+
+    def add_thumb
+      @raffle.update(thumb: @thumb)
+    end
+
+    def call
+      ActiveRecord::Base.transaction do
+        create_product
+        create_raffle!
+        add_thumb
+      end
+    end
+
+    def create_product
+      @product = ProductCreateService.call(@product_params)
+    end
+
+    def create_raffle!
+      @raffle = @product.create_raffle!(@raffle_params)
+    end
+  end
+end

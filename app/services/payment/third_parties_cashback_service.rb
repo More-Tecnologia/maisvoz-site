dabd5b4e45b96366c2b1a9820568e4448580e35c
heirian <26394609+Heirian@users.noreply.github.com>
2022-04-07 19:54:15 -0300

    Add third parties cashback service

---
+++ app/services/payment/third_parties_cashback_service.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Payment
+  class ThirdPartiesCashbackService < ApplicationService
+    def initialize(params)
+      @user = params[:user]
+      @order = params[:order]
+    end
+
+    private
+
+    def amount_to_cashback
+      @order.total_cents / 1000
+    end
+
+    def call
+      create_cashback_payment
+    end
+
+    def cashback_reason
+      FinancialReason.cashback_reason
+    end
+
+    def create_cashback_payment
+      @user.financial_transactions
+           .create!(spreader: User.find_morenwm_customer_admin,
+                    financial_reason: cashback_reason,
+                    cent_amount: amount_to_cashback,
+                    moneyflow: :credit,
+                    note: I18n.t(:third_party_payment,
+                                 order_id: @order.hashid,
+                                 beneficiary: @order.user.username))
+    end
+  end
+end

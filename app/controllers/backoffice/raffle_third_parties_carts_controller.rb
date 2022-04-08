b0c482ac7da7cc850bef0f95e3fbf6da7716c8d9
heirian <26394609+Heirian@users.noreply.github.com>
2022-04-08 15:53:59 -0300

    Minor updates

--- app/controllers/backoffice/raffle_third_parties_carts_controller.rb
+++ app/controllers/backoffice/raffle_third_parties_carts_controller.rb
@@ -3,7 +3,7 @@
 module Backoffice
   class RaffleThirdPartiesCartsController < BackofficeController
     def show
-      @q = Order.includes(:product)
+      @q = Order.includes(order_items: :product)
                 .ransack(params)
       @order = @q.result
                  .first

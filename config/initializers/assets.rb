b44d7976f93f0a5ee1b06b3c9c2582a85a97129e
Italo <italojbraga@gmail.com>
2022-04-11 15:04:00 -0300

    create-raffle-third-parties-page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -18,6 +18,7 @@ Rails.application.config.assets.precompile += %w( raffles/register-raffle.css )
 Rails.application.config.assets.precompile += %w( raffles/winners.css )
 Rails.application.config.assets.precompile += %w( raffles/tickets.css )
 Rails.application.config.assets.precompile += %w( raffles/raffles-tickets.js )
+Rails.application.config.assets.precompile += %w( raffle-third-parties-cart/index.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.

cc80105b421d03fda9f4c0dcb50c2be5a2c469a9
heirian <26394609+Heirian@users.noreply.github.com>
2022-04-13 17:16:58 -0300

    Updates raffle third parties carts controller

--- app/controllers/backoffice/raffle_third_parties_carts_controller.rb
+++ app/controllers/backoffice/raffle_third_parties_carts_controller.rb
@@ -3,10 +3,15 @@
 module Backoffice
   class RaffleThirdPartiesCartsController < BackofficeController
     def show
-      @q = Order.includes(order_items: :product)
-                .ransack(params)
-      @order = @q.result
-                 .first
+      @order = Order.joins(:products)
+                    .pending_payment
+                    .where.not(user: current_user)
+                    .where(products: { id: Product.raffle })
+                    .find_by_hashid(params[:hashid])
+
+      if @order.nil? && params[:hashid].present?
+        flash[:error] = t(:raffle_order_not_found)
+      end
     end
   end
 end

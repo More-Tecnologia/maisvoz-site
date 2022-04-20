0e93d82b61c59f55e51d78d9b5b20121be8d0b92
kenidy <kenidys@gmailcom>
2022-04-20 20:00:40 -0300

    Improve shared_helper logic

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SharedHelper
-  def active_css_class_by_path(path)
-    return 'active' if request.path == path
+  def css_class_by_path(path, class_name='active')
+    class_name if request.path == path
   end
 end

6711eae91ab3b3662ff6835eb2e63390fbb707e7
kenidy <kenidys@gmailcom>
2022-04-28 11:49:39 -0300

    Remove ads menu

--- app/assets/javascripts/click-tok-theme/tasks/index.js
+++ app/assets/javascripts/click-tok-theme/tasks/index.js
@@ -1,21 +1,3 @@
-let boxesStore = document.querySelectorAll('.box-store');
-console.log(boxesStore)
-
-
-function selectedBanner(){
-  boxesStore.forEach((item) => {
-    if(item.href == window.location.href){
-      item.classList.add('selected-background');
-    }
-    else {
-      item.classList.remove('selected-background');
-    }
-  });
-
-}
-
-selectedBanner();
-
 const canvas = document.querySelectorAll('.chartjs-render-monitor');
 const chart1 = document.querySelectorAll('#chart-1');
 const chart2 = document.querySelectorAll('#chart-2');

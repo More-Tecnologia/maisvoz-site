e5623ffbf77488daf1468dce5b11896d9ecadf53
kenidy <kenidys@gmailcom>
2022-04-28 09:19:54 -0300

    Fix script

--- app/assets/javascripts/click-tok-theme/tasks/banner.js
+++ app/assets/javascripts/click-tok-theme/tasks/banner.js
@@ -1,14 +1,17 @@
-  let canvas = document.querySelectorAll('.chartjs-render-monitor');
-  let chart1 = document.querySelectorAll('#chart-1');
-  let chart2 = document.querySelectorAll('#chart-2');
+  const canvas = document.querySelectorAll('.chartjs-render-monitor');
+  const chart1 = document.querySelectorAll('#chart-1');
+  const chart2 = document.querySelectorAll('#chart-2');
 
   canvas.forEach((canvas) => {
     canvas.style.width = "56px";
     canvas.style.height = "56px";
   });
+
   chart1.forEach((chart) => {
     chart.style = "";
   });
+
   chart2.forEach((chart) => {
     chart.style = "";
   });
+  

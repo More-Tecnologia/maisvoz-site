66d276eaf4d41ccdcff62325eac61ae0092f54c1
kenidy <kenidys@gmailcom>
2022-05-05 09:46:43 -0300

    Fix translation

--- app/helpers/backoffice/stores_helper.rb
+++ app/helpers/backoffice/stores_helper.rb
@@ -1,12 +1,12 @@
 module Backoffice
   module StoresHelper
     BANNER_ATTRIBUTE_LIST_BY_KIND = {
-      deposit: { description: :task_per_day, image: :main_photo_path, price: :price, title: :name},
+      deposit: { description: :task_per_day, image: :main_photo_path, price: :price, title: :name },
       course: { description: :course_short_description, image: :course_path, price: :price, title: :name },
       publicity: { description: :clicks, image: :main_photo_path, price: :price, title: :name },
       raffle: { description: :description, image: :raffle_path, price: :price, title: :name }
     }
-
+        
     def banner_attribute_by_noddle(banner_item, noddle)
       BANNER_ATTRIBUTE_LIST_BY_KIND[banner_item.kind.to_sym][noddle]
     end

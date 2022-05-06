c2cea6c84b8f33e2eaea7d41a0622e6e583aa496
kenidy <kenidys@gmailcom>
2022-05-06 08:06:35 -0300

    Refactor javascript

--- app/assets/javascripts/click-tok-theme/stores/shared/banner.js
+++ app/assets/javascripts/click-tok-theme/stores/shared/banner.js
@@ -1,10 +1,10 @@
-function changeSlide() {
+const changeSlide = () => {
   let nextSlider = parseInt(document.querySelector("input[name = radio-btn]:checked").id[5]) + 1;
   if (nextSlider > 4) nextSlider = 1;
   document.getElementById("radio" + nextSlider).checked = true;
 }
 
-function autoChangeSlide(seconds = 8) {
+const autoChangeSlide = (seconds = 8) => {
   setInterval(changeSlide, seconds * 1000);
 }
 

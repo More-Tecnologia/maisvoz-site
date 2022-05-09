269a853e4dc676feb741b25b449c6504faba115d
kenidy <kenidys@gmailcom>
2022-05-09 09:08:30 -0300

    Improve format_currency helper

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -6,14 +6,14 @@ module SharedHelper
   end
 
   def format_currency(value, symbol = '<b>$</b>')
+    full_number = number_to_currency(value, unit: ENV['CURRENT_CURRENCY'], precision: 2).gsub('USD ', symbol)
+    whole_number = full_number[0...-3]
+    cents_number = full_number.last(2)
 
-    split_value = number_to_currency(value, unit: ENV['CURRENT_CURRENCY'], delimiter: '',
-                                     precision: 2).gsub('USD ', symbol).sub(',','.').split('.')    
-    " #{split_value.first}<i>#{split_value.last}</i>".html_safe    
+    "#{whole_number}<i>#{cents_number}</i>".html_safe
   end
 
   def format_currency_with_separator(value, symbol = '<b>$</b>')
-    split_value = number_to_currency(value, unit: ENV['CURRENT_CURRENCY'],
-                                     precision: 2).gsub('USD ', symbol).html_safe
+    number_to_currency(value, unit: ENV['CURRENT_CURRENCY'], precision: 2).gsub('USD ', symbol).html_safe
   end
 end

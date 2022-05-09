f9b4be5603fb8d0add81edbac0b5d781c8ff8c55
kenidy <kenidys@gmailcom>
2022-05-09 19:04:15 -0300

    create logic change banner on click

--- app/assets/javascripts/click-tok-theme/stores/shared/banner.js
+++ app/assets/javascripts/click-tok-theme/stores/shared/banner.js
@@ -1,11 +1,29 @@
-const changeSlide = () => {
-  let nextSlider = parseInt(document.querySelector("input[name = radio-btn]:checked").id[5]) + 1;
-  if (nextSlider > 4) nextSlider = 1;
-  document.getElementById("radio" + nextSlider).checked = true;
+function dinamicBanner() {
+  const banner = {};
+  const bannerWrapper = document.querySelector(".store-banner-content");
+  const bannerSize = document.querySelector(".store-banner-container").offsetWidth;
+  const bannerCount = document.querySelectorAll(".store-banner-container").length;
+  const buttonPrevious = document.querySelector(".store-banner-button__left");
+  const buttonNext = document.querySelector(".store-banner-button__right");  
+  let currentBanner = 0;
+
+  banner.goNext = () => {
+    currentBanner < bannerCount-2 ? currentBanner++ : currentBanner = bannerCount-1 ;  
+    bannerWrapper.scroll(currentBanner * bannerSize, 0);
+    console.log(currentBanner);
+  };
+
+  banner.goPrevious = () => {
+    currentBanner > 0 ? currentBanner-- : currentBanner = bannerCount-1 ;  
+    bannerWrapper.scroll(currentBanner * bannerSize, 0);
+    console.log(currentBanner);
+  };
+
+  buttonPrevious.addEventListener('click', banner.goPrevious);
+  buttonNext.addEventListener('click', banner.goNext);
+
+  return banner;
 }
 
-const autoChangeSlide = (seconds = 15) => {
-  setInterval(changeSlide, seconds * 1000);
-}
+const storeBanner = dinamicBanner();
 
-autoChangeSlide();

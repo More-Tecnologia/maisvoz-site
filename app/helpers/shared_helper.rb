7b8a23e4fcbedfb3a51c6aa6edd48578bdf9d200
kenidy <kenidys@gmailcom>
2022-05-10 11:11:06 -0300

    Fix helper and banner logic

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -5,12 +5,19 @@ module SharedHelper
     class_name if request.path == path
   end
 
-  def format_currency(value, symbol = '<b>$</b>')
+  def format_usd_currency(value, symbol)
     full_number = number_to_currency(value, unit: ENV['CURRENT_CURRENCY'], precision: 2).gsub('USD ', symbol)
     whole_number = full_number[0...-3]
     cents_number = full_number.last(2)
 
     "#{whole_number}<i>#{cents_number}</i>".html_safe
+  end
+
+  def format_currency(value, currency = 'usd')
+    case currency
+    when 'usd'
+      format_usd_currency(value, '<b>$</b>')
+    end
   end
 
   def format_currency_with_separator(value, symbol = '<b>$</b>')

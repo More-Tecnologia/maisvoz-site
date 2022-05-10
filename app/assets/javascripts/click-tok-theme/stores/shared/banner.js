eb3e425b36d2486c15e70e7ef62791d7b85961ba
kenidy <kenidys@gmailcom>
2022-05-10 19:13:20 -0300

    Fix commit bug

--- app/assets/javascripts/click-tok-theme/stores/shared/banner.js
+++ app/assets/javascripts/click-tok-theme/stores/shared/banner.js
@@ -1,46 +1,96 @@
-function dinamicBanner() {
-  const banner = {};
-  const bannerWrapper = document.querySelector(".store-banner-content");
-  const bannerSize = document.querySelector(".store-banner-container").offsetWidth;
-  const bannerCount = document.querySelectorAll(".store-banner-container").length;
-  const buttonPrevious = document.querySelector(".store-banner-button__left");
-  const buttonNext = document.querySelector(".store-banner-button__right");  
-  let currentBanner = 0;
-
-  banner.goStart = () => {
-    bannerWrapper.scroll(0, 0);
+function bannerConstruct() {
+  function newElement(elementName) {
+    return document.querySelector(elementName);
   }
 
-  banner.goNext = () => {
-    if (currentBanner < bannerCount-2) {
-      currentBanner++
-      buttonPrevious.classList.add('active');
-    }else {
-      currentBanner = bannerCount-1
-      buttonNext.classList.remove('active');
-      buttonPrevious.classList.add('active');
-    }
-    bannerWrapper.scroll(currentBanner * bannerSize, 0);
+  function newElements(elementName) {
+    return document.querySelectorAll(elementName);
+  }
+
+  return {
+    btnNext: newElement(".store-banner-button__left"),
+    btnPrevious: newElement(".store-banner-button__right"),
+    content: newElement(".store-banner-content"),
+    current: 0,
+    length: newElements(".store-banner-container").length - 1,
+    slide: newElements(".store-banner-container"),
+    width: newElement(".store-banner-container").offsetWidth,
   };
-
-  banner.goPrevious = () => {
-    if(currentBanner > 1){ 
-      currentBanner--
-      buttonNext.classList.add('active');
-    }else{
-      currentBanner = 0 
-      buttonPrevious.classList.remove('active');
-      buttonNext.classList.add('active');
-    }
-    bannerWrapper.scroll(currentBanner * bannerSize, 0);
-  };
-
-  banner.goStart();
-  buttonPrevious.addEventListener('click', banner.goPrevious);
-  buttonNext.addEventListener('click', banner.goNext);
-
-  return banner;
 }
 
-const storeBanner = dinamicBanner();
+function dinamicBanner(delay) {
+  const banner = bannerConstruct();
 
+  const autoSlide = (timer = delay) => {
+    clearTimeout(banner.timer);
+    banner.timer = setTimeout(goNext, timer * 1000);
+  };
+
+  const scroll = (scrollValue) => {
+    banner.content.scroll(scrollValue * banner.width, 0);
+  };
+
+  const mouseDownHandler = (event) => {
+    banner.startPosition = event.clientX;
+    banner.isClicked = true;
+  };
+
+  const mouseMoveHandler = (event) => {
+    if (banner.isClicked && event.clientX < banner.startPosition) {
+      goNext();
+      banner.isClicked = false;
+    } else if (banner.isClicked && event.clientX > banner.startPosition) {
+      goPreview();
+      banner.isClicked = false;
+    }
+  };
+
+  const touchStartHandler = (event) => {
+    banner.startPosition = event.targetTouches[0].pageX;
+  };
+
+  const touchEndHandler = (event) => {
+    if (event.changedTouches[0].pageX < banner.startPosition) {
+      goNext();
+    } else if (event.changedTouches[0].pageX > banner.startPosition) {
+      goPreview();
+    }
+  };
+
+  const resizeHandler = () => {
+    banner.width = banner.slide.offsetWidth;
+    scroll(banner.current);
+  };
+
+  const goNext = () => {
+    if (banner.current < banner.length) {
+      banner.current++;
+    } else {
+      banner.current = 0;
+    }
+    scroll(banner.current);
+    autoSlide();
+  };
+
+  const goPreview = () => {
+    if (banner.current > 0) {
+      banner.current--;
+    } else {
+      banner.current = banner.length;
+    }
+    scroll(banner.current);
+    autoSlide();
+  };
+
+  banner.content.addEventListener("mousedown", mouseDownHandler);
+  banner.content.addEventListener("mousemove", mouseMoveHandler);
+  banner.content.addEventListener("touchstart", touchStartHandler, false);
+  banner.content.addEventListener("touchend", touchEndHandler, false);
+  banner.btnNext.addEventListener("click", goPreview);
+  banner.btnPrevious.addEventListener("click", goNext);
+  window.addEventListener("resize", resizeHandler);
+  autoSlide();
+  scroll(0);
+}
+
+dinamicBanner(10);

0a61830beeaf8bfcf3288330e6a6b2b47b7f0ddf
kendidy <kenidys@gmail.com>
2022-06-03 05:47:29 -0300

    Style the modal box

--- app/assets/javascripts/click-tok-theme/stores/raffles/raffles_tickets_agreement.js
+++ app/assets/javascripts/click-tok-theme/stores/raffles/raffles_tickets_agreement.js
@@ -4,7 +4,10 @@ const publicRegulation = {};
     body: document.querySelector("body"),
     regulation: document.querySelector(".modal-regulation"),
   };
-  const buttons = { regulation: document.querySelector(".regulation-button") };
+  const buttons = {
+    regulation: document.querySelector(".regulation-button"),
+    close: document.querySelector(".modal-close-button"),
+  };
 
   function openModalHendler() {
     containers.body.insertAdjacentHTML(
@@ -12,14 +15,15 @@ const publicRegulation = {};
       '<div onclick="publicRegulation.closeModalHandler()" class="regulation-backdrop"></div>'
     );
     containers.body.classList.add("backdrop");
-    containers.regulation.classList.add("open")
+    containers.regulation.classList.add("open");
   }
 
   publicRegulation.closeModalHandler = function closeModalHandler() {
     getElement(".regulation-backdrop").remove();
     containers.body.classList.remove("backdrop");
-    containers.regulation.classList.remove("open")
+    containers.regulation.classList.remove("open");
   };
 
   buttons.regulation.addEventListener("click", openModalHendler);
+  buttons.close.addEventListener('click', publicRegulation.closeModalHandler);
 })();

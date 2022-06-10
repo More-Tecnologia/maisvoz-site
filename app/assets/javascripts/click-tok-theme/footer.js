a8aa0c41e9faba10e4c2e6b7b14599563b1d1127
Italo <italojbraga@gmail.com>
2022-06-10 16:42:31 -0300

    Fix bug in footer menu

--- app/assets/javascripts/click-tok-theme/footer.js
+++ app/assets/javascripts/click-tok-theme/footer.js
@@ -1,161 +1,42 @@
 const list = document.querySelectorAll(".list");
-let indicator = document.querySelector(".indicator");
-
-if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-  let initialIndicator = window.innerWidth / 2;
-  let porcentIndicator = initialIndicator / 5;
-  let finalIndicator = initialIndicator - porcentIndicator;
-  indicator.style.marginLeft = `-${finalIndicator}px`;
-}
-
-function activeLink() {
-  list.forEach((item) => {
-    item.classList.remove("active");
-    this.parentElement.classList.add("active");
-  });
-}
-list.forEach((item) => {
-  item.firstElementChild.addEventListener("click", activeLink);
-  item.firstElementChild.addEventListener("click", moveMenu);
-});
-
-function moveMenu() {
-  let menu = document.querySelector(".main-list");
-  let lists = document.querySelectorAll(".list");
-  let indicator = document.querySelector(".indicator");
-  let x = 0;
-
-  if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-    x = 0;
-  }
-
-  list.forEach((item, i) => {
-    if (item.className == "list active") {
-      indicator.style.transition = "0.5s";
-      indicator.style.transform = `translateX(calc(${
-        item.offsetWidth + "px"
-      } * ${x}))`;
-    }
-    x++;
-  });
-}
-
+let local = window.location.pathname;
+console.log(local)
 function itemSelected() {
-  let links = document.querySelectorAll(".link-nav");
-  let sizeNav = document.querySelector(".list").offsetWidth;
-  let firstList = list[0];
-  let footerDashboardHome = [
-    "/",
-    "/backoffice/admin/dashboard",
-    "/backoffice/home",
-  ];
-
-  let footerTasksUsers = [
-    "/backoffice/dashboard",
-    "/backoffice/banners",
-    "/backoffice/support/users",
-  ];
-
-  let footerShop = [
-    "/backoffice/products",
-    "/backoffice/stores/games",
-    "/backoffice/stores/courses",
-    "/backoffice/stores/ads",
-    "/backoffice/cart",
-  ];
-
-  let footerFinancial = [
-    "/backoffice/financial_dashboard",
-    "/backoffice/withdrawals",
-    "/backoffice/financial_transactions",
-    "/backoffice/orders",
-    "/backoffice/bank_account/edit.1",
-    "/backoffice/withdrawals/new",
-    "/backoffice/admin/withdrawals",
-    "/backoffice/admin/credits_debits/find_user",
-    "/backoffice/admin/expenses/new",
-    "/backoffice/admin/orders",
-    "/backoffice/admin/financial_transactions",
-    "/backoffice/admin/bonus_contracts",
-  ];
-
-  let footerTeam = ["/backoffice/team_dashboard"];
-  let counterDashboard = 0;
-  let counterUsers = 1;
-  let counterShop = 2;
-  let counterFinancial = 3;
-  let counterMyTeam = 4;
-
-  if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-    counterDashboard = 0;
-    counterUsers = 1;
-    counterShop = 2;
-    counterFinancial = 3;
-    counterMyTeam = 4;
+  if (local.includes("/backoffice/home") || local.includes("/backoffice/admin/dashboard")) {
+    list[0].classList.add("menu-selected");
+  } else {
+    list[0].classList.remove("menu-selected");
   }
 
-  if (footerDashboardHome.includes(window.location.pathname)) {
-    indicator.style.transform = `translateX(calc(${
-      sizeNav + "px"
-    } * ${counterDashboard}))`;
-    indicator.style.transition = "none";
-    list[0].classList.add("active");
+  if (local.includes("/backoffice/raffles_tickets") || local.includes("/backoffice/support/users")) {
+    list[1].classList.add("menu-selected");
   } else {
-    list[0].classList.remove("active");
+    list[1].classList.remove("menu-selected");
   }
 
-  if (footerTasksUsers.includes(window.location.pathname)) {
-    indicator.style.transform = `translateX(calc(${
-      sizeNav + "px"
-    } * ${counterUsers}))`;
-    indicator.style.transition = "none";
-    list[1].classList.add("active");
+  if (local.includes("/backoffice/stores/") || local == "/") {
+    list[2].classList.add("menu-selected");
   } else {
-    list[1].classList.remove("active");
-  }
-
-  if (footerShop.includes(window.location.pathname)) {
-    indicator.style.transform = `translateX(calc(${
-      sizeNav + "px"
-    } * ${counterShop}))`;
-    indicator.style.transition = "none";
-    list[2].classList.add("active");
-  } else {
-    list[2].classList.remove("active");
-  }
-
-  if (footerFinancial.includes(window.location.pathname)) {
-    indicator.style.transform = `translateX(calc(${
-      sizeNav + "px"
-    } * ${counterFinancial}))`;
-    indicator.style.transition = "none";
-    list[3].classList.add("active");
-  } else {
-    list[3].classList.remove("active");
-  }
-
-  if (footerTeam.includes(window.location.pathname)) {
-    indicator.style.transform = `translateX(calc(${
-      sizeNav + "px"
-    } * ${counterMyTeam}))`;
-    indicator.style.transition = "none";
-    list[4].classList.add("active");
-  } else {
-    list[4].classList.remove("active");
+    list[2].classList.remove("menu-selected");
   }
 
   if (
-    footerDashboardHome.includes(window.location.pathname) == false &&
-    footerTasksUsers.includes(window.location.pathname) == false &&
-    footerShop.includes(window.location.pathname) == false &&
-    footerFinancial.includes(window.location.pathname) == false &&
-    footerTeam.includes(window.location.pathname) == false
+    local.includes("/backoffice/financial_dashboard") ||
+    local.includes("/backoffice/financial_transactions") ||
+    local.includes("/backoffice/orders") ||
+    local.includes("/backoffice/bank_account/") ||
+    local.includes("/backoffice/withdrawals") ||
+    local.includes("/backoffice/raffle_third_parties_carts")
   ) {
-    firstList.classList.add("active");
-    indicator.style.transform = `translateX(calc(${
-      sizeNav + "px"
-    } * ${counterDashboard}))`;
-    indicator.style.transition = "none";
+    list[3].classList.add("menu-selected");
+  } else {
+    list[3].classList.remove("menu-selected");
+  }
+
+  if (local.includes("/backoffice/team_dashboard")) {
+    list[4].classList.add("menu-selected");
+  } else {
+    list[4].classList.remove("menu-selected");
   }
 }
 

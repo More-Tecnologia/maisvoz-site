45a51c040d061e5fef3160e4b795e5e66583e00b
heirian <26394609+Heirian@users.noreply.github.com>
2022-07-05 09:53:29 -0300

    Add dynamic raffle winners

--- app/controllers/backoffice/raffles_controller.rb
+++ app/controllers/backoffice/raffles_controller.rb
@@ -2,13 +2,16 @@
 
 module Backoffice
   class RafflesController < BackofficeController
-    skip_before_action :authenticate_user!
+    skip_before_action :authenticate_user!, only: :winners
 
     def index; end
 
-    def agreement; end
-
-    def winners; end
+    def winners
+      @raffles = Raffle.finished
+                       .order(draw_date: :desc)
+                       .page(params[:page])
+                       .per(10)
+    end
 
     def show; end
   end

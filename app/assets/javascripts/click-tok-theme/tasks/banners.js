9fafbbbc98135d68a57d48327993e0e75b8ebd03
kenidy <kenidys@gmailcom>
2022-07-12 14:38:33 -0300

    Improve banner's behavior

--- app/assets/javascripts/click-tok-theme/tasks/banners.js
+++ app/assets/javascripts/click-tok-theme/tasks/banners.js
@@ -1,55 +1,67 @@
-  $('.dashboard-banner-container-premium').owlCarousel({
-    loop:true,
-    margin:0,
-    responsiveClass:true,
-    nav: true,
-    navText : ['<b class="banner-left-buttom-premium"><</b>',
-      '<b class="banner-right-buttom-premium">></b>'],
-    responsive:{
-      0:{
-          items:3,
-          nav:true
-      },
-      400:{
-          items:3,
-          nav:true
-      },
-        800:{
-          items:4,
-          nav:true
-      },
-      1000:{
-          items:5,
-          nav:true,
-          loop:true
-      }
-    }
-  })
-  
-  $('.dashboard-banner-container-basic').owlCarousel({
-    loop:true,
-    margin:0,
-    responsiveClass:true,
-    nav: true,
-    navText : ['<b class="banner-left-buttom"><</b>',
-      '<b class="banner-right-buttom">></b>'],
-    responsive:{
-      0:{
-          items:4,
-          nav:true
-      },
-      600:{
-          items:6,
-          nav:true
-      },
-        800:{
-          items:8,
-          nav:true
-      },
-      1000:{
-          items:10,
-          nav:true,
-          loop:true
-      }
-    }
-  })
+$(".dashboard-banner-container-premium").owlCarousel({
+  loop: true,
+  margin: 7,
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsiveClass: true,
+  nav: true,
+  navText: [
+    '<b class="banner-left-buttom-premium"><i class="fa-solid fa-caret-left nav-icon"></i></b>',
+    '<b class="banner-right-buttom-premium"><i class="fa-solid fa-caret-right nav-icon"></i></b>',
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 2,
+      nav: true,
+    },
+    800: {
+      items: 3,
+      nav: true,
+    },
+    1000: {
+      items: 4,
+      nav: true,
+      loop: true,
+    },
+    1400: {
+      items: 4,
+      nav: true,
+      loop: true,
+    },
+  },
+});
+
+$(".dashboard-banner-container-basic").owlCarousel({
+  loop: true,
+  margin: 4,
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsiveClass: true,
+  nav: false,
+  navText: [
+    '<b class="banner-left-buttom"><i class="fa-solid fa-caret-left nav-icon"></i></b>',
+    '<b class="banner-right-buttom"><i class="fa-solid fa-caret-right nav-icon"></i></b>',
+  ],
+  responsive: {
+    0: {
+      items: 3,
+      nav: false,
+    },
+    600: {
+      items: 6,
+      nav: false,
+    },
+    800: {
+      items: 8,
+      nav: false,
+    },
+    1000: {
+      items: 10,
+      nav: false,
+    },
+  },
+});
